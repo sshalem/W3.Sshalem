1aaa9e590f891cfce16360d936efb10683f24cd1
shabtay shalem <shabtay.shalem@gmail.com>
2025-06-25 01:43:31 +0300

    25-06-2025  1:43

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -9,7 +9,10 @@ const MainLayout = () => {
         <div className="mx-auto w-11/12">
           <Link to={"/"}>
             <div className="my-2 inline-flex select-none gap-2 rounded-md bg-blue-600 p-2 text-white hover:bg-blue-400">
-              <SiStudyverse className="self-center" /> W3.Sshalem tutorials
+              <SiStudyverse className="self-center" />
+              <div>
+                W<sup>3</sup>.Sshalem
+              </div>
             </div>
           </Link>
         </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,22 @@
+// import devloperImg from "../assets/developer.png";
 const Home = () => {
-  return <h1 className="text-3xl">Welcome to sshalem Software tutorials ... </h1>;
+  return (
+    <>
+      <article className="relative">
+        <div className="header">
+          <div className="mx-auto flex h-[29rem] w-11/12 flex-col items-center justify-evenly text-5xl font-medium text-white">
+            <div>
+              W<sup>3</sup>
+              Sshalem Software tutorials
+            </div>
+            <div className="css-developer h-40 w-40 self-center"></div>
+            <div className="self-center text-4xl">Shabtay Shalem</div>
+            <div className="self-center text-4xl">Full Stack Developer</div>
+          </div>
+        </div>
+      </article>
+    </>
+  );
 };
 
 export default Home;
