0267762f3015c1d11481d157276f591f5f3dbcdf
shabtay shalem <shabtay.shalem@gmail.com>
2025-06-25 01:46:33 +0300

    25-06-2025  1:46

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,8 +6,7 @@ const Home = () => {
         <div className="header">
           <div className="mx-auto flex h-[29rem] w-11/12 flex-col items-center justify-evenly text-5xl font-medium text-white">
             <div>
-              W<sup>3</sup>
-              Sshalem Software tutorials
+              W<sup>3</sup> Sshalem Software tutorials
             </div>
             <div className="css-developer h-40 w-40 self-center"></div>
             <div className="self-center text-4xl">Shabtay Shalem</div>
