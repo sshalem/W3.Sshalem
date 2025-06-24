5e295d1e2f71365873bb11f5288146ff391bcef4
sshalem <shabtay.shalem@gmail.com>
2025-06-24 19:21:01 +0300

    24-06-2025 19:21

--- src/components/LeftColumnLayout.tsx
+++ src/components/LeftColumnLayout.tsx
@@ -2,7 +2,7 @@ const LeftColumnLayout = ({ children }: React.PropsWithChildren) => {
   return (
     <article className="sticky top-[96px] max-h-[calc(100vh-6rem)] overflow-auto">
       {/* Start gap divider   */}
-      <div className="h-3 bg-gray-100"></div>
+      <div className="h-3 bg-[#E7E9EB]"></div>
       {/* End gap divider   */}
       {children}
     </article>
--- src/components/SideBarLink.tsx
+++ src/components/SideBarLink.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const SideBarLink = ({ pageName, internalLink }: { pageName: string; internalLink: string }) => {
   return (
     <Link to={internalLink}>
-      <div className="bg-gray-100 text-base hover:bg-gray-300">
+      <div className="bg-[#E7E9EB] text-base hover:bg-gray-300">
         <div className="ml-8">{pageName}</div>
       </div>
     </Link>
