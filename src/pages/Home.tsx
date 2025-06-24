b754195f681be1f0b9f9a436c333aa9d564b6c64
shabtay shalem <shabtay.shalem@gmail.com>
2025-06-25 01:48:28 +0300

    25-06-2025  1:48

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,16 @@
-// import devloperImg from "../assets/developer.png";
 const Home = () => {
   return (
     <>
-      <article className="relative">
-        <div className="header">
-          <div className="mx-auto flex h-[29rem] w-11/12 flex-col items-center justify-evenly text-5xl font-medium text-white">
-            <div>
-              W<sup>3</sup> Sshalem Software tutorials
-            </div>
-            <div className="css-developer h-40 w-40 self-center"></div>
-            <div className="self-center text-4xl">Shabtay Shalem</div>
-            <div className="self-center text-4xl">Full Stack Developer</div>
+      <div className="header">
+        <div className="mx-auto flex h-[29rem] w-11/12 flex-col items-center justify-evenly text-5xl font-medium text-white">
+          <div>
+            W<sup>3</sup> Sshalem Software tutorials
           </div>
+          <div className="css-developer h-40 w-40 self-center"></div>
+          <div className="self-center text-4xl">Shabtay Shalem</div>
+          <div className="self-center text-4xl">Full Stack Developer</div>
         </div>
-      </article>
+      </div>
     </>
   );
 };
