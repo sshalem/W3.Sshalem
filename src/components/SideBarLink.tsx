29527088a492095919849b91d5c7bcc6d2680a9d
shabtay shalem <shabtay.shalem@gmail.com>
2025-06-26 00:51:56 +0300

    26-06-2025  0:51

--- src/components/SideBarLink.tsx
+++ src/components/SideBarLink.tsx
@@ -5,10 +5,20 @@ const SideBarLink = ({ pageName, internalLink }: { pageName: string; internalLin
 
   return (
     <Link to={internalLink}>
-      {/* {console.log(location.pathname.substring(8))}
-      {console.log(location.pathname)} */}
+      {console.log(location.pathname.substring(8))}
+      {/* {console.log(location.pathname)} */}
+      {console.log(internalLink)}
       <div className="text-[15px] text-base leading-[1.65]">
-        <div className={`${location.pathname.substring(8) === internalLink ? `bg-blue-600 text-white` : internalLink === "." ? `bg-blue-600 text-white` : `hover:bg-gray-300`} pl-8`}>{pageName}</div>
+        <div
+          className={`${location.pathname.substring(8) === internalLink ? `bg-blue-600 text-white` : `hover:bg-gray-300`} pl-8`}
+        >
+          {pageName}
+        </div>
+        {/* <div
+          className={`${internalLink === location.pathname.substring(8) ? `bg-blue-600 text-white` : `hover:bg-gray-300`} pl-8`}
+        >
+          {pageName}
+        </div> */}
 
         {/* <div className={`${location.pathname === `/spring/${internalLink}` ? `bg-blue-600 text-white` : `hover:bg-gray-300`} pl-8`}>{pageName}</div> */}
       </div>
