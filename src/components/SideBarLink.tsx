69ca9e9a2d941132edfdceaa782efe0ec6bcf6ee
sshalem <shabtay.shalem@gmail.com>
2025-06-26 13:33:48 +0300

    26-06-2025 13:33

--- src/components/SideBarLink.tsx
+++ src/components/SideBarLink.tsx
@@ -6,29 +6,13 @@ const SideBarLink = ({ pageName, internalLink }: { pageName: string; internalLin
   return (
     <Link to={internalLink}>
       <div className="text-[15px] text-base leading-[1.65]">
-        {/* <div
-          className={`${location.pathname.substring(8).length === 0 && internalLink === "." ? `bg-blue-600 text-white` : `hover:bg-gray-300`} pl-8`}
-        >
-          {pageName}
-        </div> */}
         {location.pathname.substring(8).length === 0 && internalLink === "." ? (
-          <>
-            <div className="bg-blue-600 pl-8 text-white">{pageName}</div>
-          </>
+          <div className="bg-blue-600 pl-8 text-white">{pageName}</div>
+        ) : location.pathname.substring(8) === internalLink ? (
+          <div className="bg-blue-600 pl-8 text-white">{pageName}</div>
         ) : (
-          <>
-            <div className="hover:bg-gray-300">{""}</div>
-          </>
+          <div className="pl-8 hover:bg-gray-300">{pageName}</div>
         )}
-
-        <div className={`${location.pathname.substring(8) === internalLink ? `bg-blue-600 text-white` : `hover:bg-gray-300`} pl-8`}>{pageName}</div>
-        {/* <div
-          className={`${internalLink === location.pathname.substring(8) ? `bg-blue-600 text-white` : `hover:bg-gray-300`} pl-8`}
-        >
-          {pageName}
-        </div> */}
-
-        {/* <div className={`${location.pathname === `/spring/${internalLink}` ? `bg-blue-600 text-white` : `hover:bg-gray-300`} pl-8`}>{pageName}</div> */}
       </div>
     </Link>
   );
