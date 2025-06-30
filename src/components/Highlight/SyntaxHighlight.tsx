bc901024463d8157e6f3d94bbd2ce7aa5e582442
sshalem <shabtay.shalem@gmail.com>
2025-06-30 19:35:14 +0300

    30-06-2025 19:35

---
+++ src/components/Highlight/SyntaxHighlight.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SyntaxHighlight = ({ syntax }: { syntax: string }) => {
+  return <span>SyntaxHighlight</span>;
+};
+
+export default SyntaxHighlight;
+
+// color: #009;
+// padding: 2px 6px;
