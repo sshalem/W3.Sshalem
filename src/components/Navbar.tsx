b28e10796ae01d413084199e838e3d58b1e0b91f
Shabtay Shalem <shabtay.shalem@asml.com>
2025-07-09 01:46:26 +0300

    07-09-2025  1:46

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,27 +44,26 @@ const Navbar = () => {
   };
 
   return (
-    <div className="fixed top-14 h-[33px] w-full select-none bg-gray-800 uppercase tracking-wider text-white">
+    <div className="top-nav-font fixed top-14 h-[33px] w-full select-none bg-gray-800 text-[13px] uppercase tracking-wider text-white">
       <nav className="fixed left-0 right-0 top-14 z-[3] m-auto h-[33px] w-full max-w-[1750px] align-middle">
+        {/* left Scroll button */}
         {enableLeftScrolling && (
           <button className="absolute left-0 top-0 h-[33px] w-10 bg-slate-400 px-3 hover:bg-slate-500" onMouseDown={handleLeftScrollButton}>
             <FaAngleLeft />
           </button>
         )}
-        <div className="flex overflow-hidden" ref={navRef}>
+        <div className="flex h-[33px] overflow-hidden" ref={navRef}>
           {links.map((subject, index) => {
             // console.log(location);
             return (
               <NavLink to={`${subject}`} key={index}>
                 {/* I substring subject from the 1 digit , since I dont want to have the '/' in the navbar for each link */}
-                <div className={`${location.pathname === subject ? `bg-blue-600` : `hover:bg-slate-700`} inline-block h-[33px] px-3`}>
-                  <div className="inline-block align-sub">{subject.substring(1, 50)}</div>
-                </div>
+                <div className={`${location.pathname === subject ? `bg-blue-600` : `hover:bg-slate-700`} px-3 py-2`}>{subject.substring(1, 50)}</div>
               </NavLink>
             );
           })}
         </div>
-
+        {/* Right Scroll button */}
         <button className="absolute right-0 top-0 h-[33px] w-10 bg-slate-400 px-3 hover:bg-slate-500" onMouseDown={handleRightScrollButton}>
           <FaAngleRight />
         </button>
