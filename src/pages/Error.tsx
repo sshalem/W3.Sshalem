671b9656cc0915288c155a04c343e13d57c392dd
shabtay shalem <shabtay.shalem@gmail.com>
2025-07-08 17:48:51 +0300

    08-07-2025 17:48

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,10 +1,31 @@
-import { NavLink } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Error = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log(location);
+  }, []);
+
   return (
-    <div>
-      <NavLink to="/">back home</NavLink>
-    </div>
+    <section className="mx-auto max-w-[1750px]">
+      <article className="flex h-[90vh] max-h-[90vh] flex-col items-center justify-center border-2">
+        <div className="p-2 text-4xl">url is not familiar</div>
+        <button
+          className="text-3xl text-blue-400 underline"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          Go back
+        </button>
+        {/* <NavLink to="/" className={`text-3xl text-blue-400 underline`}>
+          back home
+        </NavLink> */}
+      </article>
+    </section>
   );
 };
 
