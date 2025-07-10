96c2bccdef8ce2d03a0a13d545aabcef8e8148fc
shabtay shalem <shabtay.shalem@gmail.com>
2025-07-11 01:35:43 +0300

    11-07-2025  1:35

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,7 @@ import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 // https://medium.com/@rexosariemen/implementing-horizontal-scroll-buttons-in-react-61e0bb431be
 
 const Navbar = () => {
+  const [showScrollIcons, setShowScrollIcons] = useState<boolean>(false);
   const [enableLeftScrolling, setEnableLeftScrolling] = useState<boolean>(false);
   const [enableRightScrolling, setEnableRightScrolling] = useState<boolean>(true);
 
@@ -62,30 +63,65 @@ const Navbar = () => {
     }
   };
 
-  const determineScrollbar = () => {
-    console.log(window.innerWidth);
-    console.log(navRef);
-  };
-
   useEffect(() => {
-    window.addEventListener("resize", determineScrollbar);
-    return () => window.removeEventListener("resize", determineScrollbar);
+    const innerWidthCheck = () => {
+      // console.log(navRef);
+      // console.log(window.innerWidth + " - window innerWidth");
+      // console.log(navRef.current?.clientWidth + " - clientWidth");
+      // console.log(navRef.current?.scrollWidth + " - scrollWidth");
+
+      if (navRef.current !== null) {
+        if (navRef.current.scrollWidth - window.innerWidth > 0) {
+          console.log(navRef.current.scrollWidth - window.innerWidth > 0);
+          // I store in session storage the `showScrollIcons`
+          // beacuse , If i go to unkonwn url , I land on the error page
+          // When I pres go back on error page , I'm back to the navbar
+          // But I loose the state of `showScrollIcons`
+          // Thus , I keep the state of `showScrollIcons` in sessionStorage
+          sessionStorage.setItem("showScrollIcons", "true");
+          setShowScrollIcons(true);
+        } else {
+          console.log(navRef.current.scrollWidth - window.innerWidth > 0);
+          sessionStorage.setItem("showScrollIcons", "false");
+          setShowScrollIcons(false);
+        }
+      }
+    };
+
+    window.addEventListener("resize", innerWidthCheck);
+    return () => window.removeEventListener("resize", innerWidthCheck);
+  }, []);
+
+  // this useEffect :
+  // loads from sessionStorage the showScrollIcons (Since I loose the state of it , once I land on the error page)
+  // and set the original state of showScrollIcons
+  useEffect(() => {
+    if (sessionStorage.getItem("showScrollIcons") !== null) {
+      const value = JSON.parse(sessionStorage.getItem("showScrollIcons") as string);
+      setShowScrollIcons(value);
+    } else {
+      console.log('sessionStorage.getItem("showScrollIcons") retruned null');
+    }
   }, []);
 
   return (
     <div className="top-nav-font fixed top-14 h-[33px] w-full select-none bg-gray-800 text-[13px] uppercase tracking-wider text-white">
       <nav className="fixed left-0 right-0 top-14 z-[3] m-auto h-[33px] w-full max-w-[1750px] align-middle">
         {/* left Scroll button */}
-        {enableLeftScrolling && (
-          <button
-            className="css-blur-bg-left absolute left-14 top-0 h-[33px] w-10 bg-slate-400 px-3 hover:bg-slate-500 sm:left-12 md:left-0"
-            onMouseDown={startLeftScroll}
-            onMouseUp={stopLeftScroll}
-            onMouseLeave={stopLeftScroll}
-          >
-            <FaAngleLeft />
-          </button>
-        )}
+
+        {showScrollIcons
+          ? enableLeftScrolling && (
+              <button
+                className="css-blur-bg-left absolute left-14 top-0 h-[33px] w-10 bg-slate-400 px-3 hover:bg-slate-500 sm:left-12 md:left-0"
+                onMouseDown={startLeftScroll}
+                onMouseUp={stopLeftScroll}
+                onMouseLeave={stopLeftScroll}
+              >
+                <FaAngleLeft />
+              </button>
+            )
+          : ""}
+
         <div className="ml-14 flex h-[33px] overflow-hidden sm:ml-14 md:ml-0 lg:ml-0" ref={navRef}>
           {links.map((subject, index) => {
             // console.log(location);
@@ -98,16 +134,19 @@ const Navbar = () => {
           })}
         </div>
         {/* Right Scroll button */}
-        {enableRightScrolling && (
-          <button
-            className="css-blur-bg-right absolute right-0 top-0 h-[33px] w-10 bg-slate-400 px-3 hover:bg-slate-500"
-            onMouseDown={startRightScroll}
-            onMouseUp={stopRightScroll}
-            onMouseLeave={stopRightScroll}
-          >
-            <FaAngleRight />
-          </button>
-        )}
+
+        {showScrollIcons
+          ? enableRightScrolling && (
+              <button
+                className="css-blur-bg-right absolute right-0 top-0 h-[33px] w-10 bg-slate-400 px-3 hover:bg-slate-500"
+                onMouseDown={startRightScroll}
+                onMouseUp={stopRightScroll}
+                onMouseLeave={stopRightScroll}
+              >
+                <FaAngleRight />
+              </button>
+            )
+          : ""}
       </nav>
     </div>
   );
