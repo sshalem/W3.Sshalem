4a0ea3f603a93c74ac3db7de5d8b06e6091490c9
shabtay shalem <shabtay.shalem@gmail.com>
2025-07-21 13:26:55 +0300

    21-07-2025 13:26

--- src/components/SpringComponents/ApplicationProperties/Postgresql.tsx
+++ src/components/SpringComponents/ApplicationProperties/Postgresql.tsx
@@ -2,6 +2,12 @@ import { useEffect, useRef, useState } from "react";
 import { ApplicationPropertiesHighlight } from "../../Highlight";
 import InstallPostgresStandalone from "./PostgreSqlSections/InstallPostgresStandalone";
 
+const anchorLinks = new Map<string, string>();
+anchorLinks.set("install_postgresql_standalone_on_windows", "install postgresql standalone on windows");
+// anchorLinks.set("", "");
+// anchorLinks.set("", "");
+// anchorLinks.set("", "");
+
 const Postgresql = () => {
   const [showContent, setShowContent] = useState<boolean>(true);
   const [contentHeight, setContentHeight] = useState<number>();
@@ -40,46 +46,20 @@ const Postgresql = () => {
           style={showContent ? { height: `${contentHeight}px` } : { height: "0px" }}
           className={`${showContent ? "pt-3" : "py-0"} overflow-hidden bg-slate-200 px-1 text-xs lowercase text-teal-700 transition-[height] duration-100 ease-in-out`}
         >
-          <li>
-            <a href="#Install_PostgreSql_standalone_on_windows" className="hover:underline">
-              Install PostgreSql standalone on windows
-            </a>
-          </li>
+          {[...anchorLinks.entries()].map(([key, value]) => {
+            return (
+              <li key={key}>
+                <a href={`#${key}`} className="hover:underline">
+                  {value}
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </article>
       {/* End Contents */}
 
-      {/*  */}
-      {/*  */}
-
-      <InstallPostgresStandalone idAnchor="Install_PostgreSql_standalone_on_windows"></InstallPostgresStandalone>
-      {/* <article className="my-5 scroll-mt-[1.5rem]" id="Install_PostgreSql_standalone_on_windows">
-        <div className="mb-8 inline-block bg-gray-800 px-2 py-2 font-semibold capitalize tracking-wider text-white">
-          Install PostgreSql standalone on windows
-        </div>
-        <div className="my-4">Links :</div>
-        <ul className="mx-8">
-          <li className="list-disc">
-            <a href="https://www.youtube.com/watch?v=btxwPwnU-oI" target="_blank" className="text-blue-500">
-              https://www.youtube.com/watch?v=btxwPwnU-oI
-            </a>
-          </li>
-          <li className="list-disc">
-            <a href="https://roytuts.com/how-to-install-postgresql-zip-archive-in-windows/" target="_blank" className="text-blue-500">
-              https://roytuts.com/how-to-install-postgresql-zip-archive-in-windows/
-            </a>
-          </li>
-        </ul>
-        <div className="my-4 text-xl">Steps</div>
-        <ul className="mx-8">
-          <li className="list-decimal">Download the zip file</li>
-          <li className="list-decimal">Uninstall it , it will uninstall it to folder pgsql .</li>
-          <li className="list-decimal">Create a new folder as pgsql_data </li>
-        </ul>
-      </article> */}
-
-      {/*  */}
-      {/*  */}
+      <InstallPostgresStandalone idAnchor="Install_PostgreSql_standalone_on_windows" />
 
       <div className="my-8 h-4">{/* {this div is only for dividing} */}</div>
     </section>
