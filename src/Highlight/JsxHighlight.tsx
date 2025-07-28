61b014548a057bed65b4ec71a6fd4f05d7247503
shabtay shalem <shabtay.shalem@gmail.com>
2025-07-28 13:06:21 +0300

    28-07-2025 13:06

--- src/Highlight/JsxHighlight.tsx
+++ src/Highlight/JsxHighlight.tsx
@@ -1,6 +1,7 @@
 // import { Prism as PrismSyntaxHighlighter } from "react-syntax-highlighter";
 // import { vscDarkPlus as JSX_STYLE } from "react-syntax-highlighter/dist/esm/styles/prism";
 
+import { useState } from "react";
 import { PrismLight as PrismLightSyntaxHighlighter } from "react-syntax-highlighter";
 import jsx from "react-syntax-highlighter/dist/esm/languages/prism/jsx";
 import vscDarkPlus from "react-syntax-highlighter/dist/esm/styles/prism/vsc-dark-plus";
@@ -8,20 +9,38 @@ import vscDarkPlus from "react-syntax-highlighter/dist/esm/styles/prism/vsc-dark
 PrismLightSyntaxHighlighter.registerLanguage("jsx", jsx);
 
 const JsxHighlight = ({ jsxCode }: { jsxCode: string }) => {
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+
+  function handleCopied() {
+    navigator.clipboard.writeText(jsxCode);
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  }
   return (
-    <div>
-      <PrismLightSyntaxHighlighter
-        language="jsx"
-        style={vscDarkPlus}
-        showLineNumbers={true}
-        customStyle={{
-          fontSizeAdjust: "0.57",
-          // width: "60rem",
-          display: "flex",
-        }}
+    <div className="relative">
+      <button
+        className="absolute right-[1rem] top-[0.35rem] border-2 border-black bg-[#fbfbfb] px-1 font-mono text-sm text-black transition-colors duration-100 ease-in-out hover:bg-black hover:text-white active:bg-gray-500 active:text-white"
+        onClick={handleCopied}
       >
-        {jsxCode}
-      </PrismLightSyntaxHighlighter>
+        {isCopied ? "copied" : "copy"}
+      </button>
+
+      <div>
+        <PrismLightSyntaxHighlighter
+          language="jsx"
+          style={vscDarkPlus}
+          showLineNumbers={true}
+          customStyle={{
+            fontSizeAdjust: "0.57",
+            // width: "60rem",
+            display: "flex",
+          }}
+        >
+          {jsxCode}
+        </PrismLightSyntaxHighlighter>
+      </div>
     </div>
   );
 };
--- src/Highlight/XmlHighlight.tsx
+++ src/Highlight/XmlHighlight.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { PrismLight as PrismLightSyntaxHighlighter } from "react-syntax-highlighter";
 import xml from "react-syntax-highlighter/dist/esm/languages/prism/xml-doc";
 import {
@@ -11,24 +12,41 @@ import {
 PrismLightSyntaxHighlighter.registerLanguage("xml", xml);
 
 const XmlHighlight = ({ xmlCode }: { xmlCode: string }) => {
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+
+  function handleCopied() {
+    navigator.clipboard.writeText(xmlCode);
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  }
   return (
-    <div>
-      <PrismLightSyntaxHighlighter
-        language="xml"
-        style={XML_STYLE}
-        // showLineNumbers={true}
-        customStyle={{
-          fontSize: "0.85rem",
-          backgroundColor: "#fbfbfb",
-          outlineStyle: "solid",
-          outlineWidth: "1px",
-          outlineColor: "#dddddd",
-          // width: "60rem",
-          display: "flex",
-        }}
+    <div className="relative">
+      <button
+        className="absolute right-[1rem] top-[0.35rem] border-2 border-black bg-[#fbfbfb] px-1 font-mono text-sm text-black transition-colors duration-100 ease-in-out hover:bg-black hover:text-white active:bg-gray-500 active:text-white"
+        onClick={handleCopied}
       >
-        {xmlCode}
-      </PrismLightSyntaxHighlighter>
+        {isCopied ? "copied" : "copy"}
+      </button>
+      <div>
+        <PrismLightSyntaxHighlighter
+          language="xml"
+          style={XML_STYLE}
+          // showLineNumbers={true}
+          customStyle={{
+            fontSize: "0.85rem",
+            backgroundColor: "#fbfbfb",
+            outlineStyle: "solid",
+            outlineWidth: "1px",
+            outlineColor: "#dddddd",
+            // width: "60rem",
+            display: "flex",
+          }}
+        >
+          {xmlCode}
+        </PrismLightSyntaxHighlighter>
+      </div>
     </div>
   );
 };
