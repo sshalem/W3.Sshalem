97c45e8d6e3259782446833a21dd98c1eef81b5b
shabtay shalem <shabtay.shalem@gmail.com>
2025-07-28 18:09:53 +0300

    28-07-2025 18:09

--- src/Highlight/Span.tsx
+++ src/Highlight/Span.tsx
@@ -2,7 +2,7 @@ const Span = ({ children }: React.PropsWithChildren) => {
   return (
     <span>
       &nbsp;
-      <span className="border-[1px] border-gray-300 px-[5px] py-[0px] font-mono text-[#009]">{children}</span>&nbsp;
+      <span className="border-[1px] border-gray-300 px-[5px] py-[1px] font-mono text-[#009]">{children}</span>&nbsp;
     </span>
   );
 };
--- src/SpringComponents/ApplicationPropsPkg/DBInitialization/DBInitPlatform.tsx
+++ src/SpringComponents/ApplicationPropsPkg/DBInitialization/DBInitPlatform.tsx
@@ -1,7 +1,22 @@
 import { MainChildArea } from "../../../components";
+import { Span } from "../../../Highlight";
 
 const DBInitPlatform = ({ anchor }: { anchor: string }) => {
-  return <MainChildArea anchor={anchor}></MainChildArea>;
+  return (
+    <MainChildArea anchor={anchor}>
+      The <Span>spring.sql.init.platform</Span> property in Spring Boot is used to select a platform-specific SQL script (like
+      <Span>
+        schema-{`<platform>`}
+        .sql
+      </Span>
+      or <br />
+      <Span>
+        data-
+        {`<platform>`}.sql
+      </Span>
+      ) during database initialization.
+    </MainChildArea>
+  );
 };
 
 export default DBInitPlatform;
