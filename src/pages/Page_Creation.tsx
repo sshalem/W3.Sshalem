8b8bede7076b4c1353dce56ba2af92f862b5ea6d
shabtay shalem <shabtay.shalem@gmail.com>
2025-08-03 16:58:58 +0300

    03-08-2025 16:58

--- src/pages/Page_Creation.tsx
+++ src/pages/Page_Creation.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { GridLayout, SideBarLink } from "../components";
 import { FaBars } from "react-icons/fa";
 import { useEffect, useState } from "react";
