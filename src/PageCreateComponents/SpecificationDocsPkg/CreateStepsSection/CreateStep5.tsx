2050d31349244b614f6e8136601cad437717f11f
shabtay shalem <shabtay.shalem@gmail.com>
2025-08-04 19:55:21 +0300

    04-08-2025 19:55

--- src/PageCreateComponents/SpecificationDocsPkg/CreateStepsSection/CreateStep5.tsx
+++ src/PageCreateComponents/SpecificationDocsPkg/CreateStepsSection/CreateStep5.tsx
@@ -174,57 +174,6 @@ const C_plus_plus = () => {
 
 export default C_plus_plus;`;
 
-const jsxCode_DropDown = `import { useEffect, useRef, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { SideDropdownLink, SideDropDownTopic } from "../../components";
-
-const DropDownAdvancedTopics = () => {
-  const [showList, setShowList] = useState<boolean>(false);
-  const [listHeight, setListHeight] = useState<number>();
-
-  let location = useLocation();
-
-  const divRef = useRef<HTMLDivElement | null>(null);
-
-  const handleOpenList = () => {
-    setShowList(!showList);
-    if (divRef.current !== null) {
-      setListHeight(divRef.current.scrollHeight);
-    }
-  };
-
-  useEffect(() => {
-    if (location.pathname.includes("BBBB")) {
-      if (location.pathname.split("/")[3] === undefined) {
-        // do nothing , this way I prevent the re-render of  setShowList(true);
-      } else {
-        setShowList(true);
-      }
-      if (divRef.current !== null) {
-        setListHeight(divRef.current.scrollHeight);
-      }
-    } else {
-      setShowList(false);
-    }
-  }, [location.pathname]);
-
-  return (
-    <section>
-      <SideDropDownTopic showList={showList} handleOpenList={handleOpenList} internalLink="/AAAA/BBBB" pageName="home" />
-
-      <div
-        style={showList ? { height: \`\${listHeight}px\` } : { height: "0px" }}
-        className={\`overflow-hidden bg-white transition-[height] duration-100 ease-in-out\`}
-        ref={divRef}
-      >
-        <SideDropdownLink pageName="sidebar link name" internalLink="/AAAA/XXXX/cccc" />
-      </div>
-    </section>
-  );
-};
-
-export default DropDownAdvancedTopics;`;
-
 const jsxCode_AppAddedChildren = `        {
           path: "c++",
           element: <C_plus_plus />,
