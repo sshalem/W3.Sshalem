ae6a71c436c2acae3b3622bb209a6fb6169d1d21
Shabtay Shalem <shabtay.shalem@asml.com>
2025-08-10 05:12:49 +0300

    08-10-2025  5:12

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export { default as MainLayout } from "./MainLayout";
-import SpanGreen from "./Highlight/SpanGreen";
 export { default as TopNavbar } from "./TopNavbar";
 export { default as Navbar } from "./Navbar";
 export { default as SideNavbar } from "./SideNavbar";
