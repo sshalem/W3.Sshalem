1c96668ebe134db0b91918b8eab47db4b00a98c8
Shabtay Shalem <shabtay.shalem@asml.com>
2025-08-10 23:59:58 +0300

    08-10-2025 23:59

--- src/components/ContentAnchor.tsx
+++ src/components/ContentAnchor.tsx
@@ -1,5 +1,5 @@
 const ContentAnchor = ({ anchor }: { anchor: string }) => {
-  return <div className="mb-8 inline-block bg-gray-800 px-2 py-2 font-semibold capitalize tracking-wider text-white">{anchor}</div>;
+  return <div className="mb-8 inline-block rounded-md bg-gray-800 px-2 py-2 font-semibold capitalize tracking-wider text-white">{anchor}</div>;
 };
 
 export default ContentAnchor;
