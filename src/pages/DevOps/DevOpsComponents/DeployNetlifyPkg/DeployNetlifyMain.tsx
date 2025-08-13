90a997406e4bcc9654360dfc6ea9583d8d0ab0b1
shabtay shalem <shabtay.shalem@gmail.com>
2025-08-13 19:35:53 +0300

    13-08-2025 19:35

--- src/pages/DevOps/DevOpsComponents/DeployNetlifyPkg/DeployNetlifyMain.tsx
+++ src/pages/DevOps/DevOpsComponents/DeployNetlifyPkg/DeployNetlifyMain.tsx
@@ -1,9 +1,10 @@
 import { MainChildArea } from "../../../../components";
+import { SpanBlue } from "../../../../components/Highlight";
 
 const DeployNetlifyMain = ({ anchor }: { anchor: string }) => {
   return (
     <MainChildArea anchor={anchor}>
-      <article>
+      <article className="mb-4">
         See link{" "}
         <a href="https://www.freecodecamp.org/news/how-to-deploy-react-router-based-app-to-netlify/" target="_blank" className="text-blue-600">
           https://www.freecodecamp.org/news/how-to-deploy-react-router-based-app-to-netlify/
@@ -15,6 +16,27 @@ const DeployNetlifyMain = ({ anchor }: { anchor: string }) => {
         But when we deploy the application on any server, directly accessing the route will send the request to the server itself (Netlify in our
         case).
       </div>
+      <ul className="my-4 ml-8 list-decimal">
+        <li className="my-1">
+          Create a new file with the name <SpanBlue>_redirects</SpanBlue>
+        </li>
+        <li className="my-1">
+          Place the file under <SpanBlue>public</SpanBlue> folder of the project
+        </li>
+        <li className="my-1">
+          add the following contents inside it <SpanBlue>/* /index.html 200</SpanBlue>
+        </li>
+        <li className="my-1">
+          Here, we're telling Netlify to redirect all the routes to the <SpanBlue>index.html</SpanBlue> file.
+        </li>
+        <li className="my-1">
+          The <SpanBlue>index.html</SpanBlue> file contains our entire React app code
+        </li>
+        <li className="my-1">
+          And as routing is handled by our React app which is contained in the index.html file, our application will work without a page not found
+          issue.
+        </li>
+      </ul>
     </MainChildArea>
   );
 };
