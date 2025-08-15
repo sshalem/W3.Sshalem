95761fd384e113e27b76287504daa5d18e6c7e97
Shabtay Shalem <shabtay.shalem@asml.com>
2025-08-15 12:22:47 +0300

    08-15-2025 12:22

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,16 +1,19 @@
-import aboutImg from "../assets/header_banner.jpg";
+import Shabtay_1 from "../assets/Shabtay_1.jpg";
 
 const AboutMe = () => {
   return (
     <section className="bg-white px-0 py-8" id="about">
       <div className="about__center">
         <article className="about-img">
-          <img src={aboutImg} alt="about-img" className="about-img__photo" />
+          <img src={Shabtay_1} alt="about-img" className="about-img__photo" />
         </article>
         <article className="about-info">
           <div className="about-info__container">
             <h1 className="about-info__title">about me</h1>
-            <p className="about-info__text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut nulla totam reprehenderit similique vel placeat ipsum natus eaque libero nam.</p>
+            <p className="about-info__text">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut nulla totam reprehenderit similique vel placeat ipsum natus eaque libero
+              nam.
+            </p>
             <div className="about-info__links">
               <a href="#" className="about-info__single-link dark-btn">
                 hire me
