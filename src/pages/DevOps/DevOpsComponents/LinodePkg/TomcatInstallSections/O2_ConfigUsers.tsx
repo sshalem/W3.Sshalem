22d22f290b9975410c1f2a640f30edfb5947ebf3
Shabtay Shalem <shabtay.shalem@asml.com>
2025-08-15 16:58:56 +0300

    08-15-2025 16:58

--- src/pages/DevOps/DevOpsComponents/LinodePkg/TomcatInstallSections/O2_ConfigUsers.tsx
+++ src/pages/DevOps/DevOpsComponents/LinodePkg/TomcatInstallSections/O2_ConfigUsers.tsx
@@ -1,6 +1,7 @@
-import { MainChildArea } from "../../../../../components";
-import { DivDoubleBorder, SpanGreen } from "../../../../../components/Highlight";
+import { IMG, MainChildArea } from "../../../../../components";
+import { DivDoubleBorder, SpanBlue, SpanGreen } from "../../../../../components/Highlight";
 import LinuxHighlight from "../../../../../components/Highlight/LinuxHighlight";
+import tomcat_6 from "../../../../../assets/tomcat_6.jpg";
 
 const O2_ConfigUsers = ({ anchor }: { anchor: string }) => {
   return (
@@ -15,29 +16,29 @@ const O2_ConfigUsers = ({ anchor }: { anchor: string }) => {
         </li>
         <LinuxHighlight bashCode={config_tomcat_1}></LinuxHighlight>
         <li className="my-1">
-          to setup a tomcat user, as manager & admin <SpanGreen>username="sshb"</SpanGreen> <SpanGreen>password="123"</SpanGreen> , paste code below
+          to setup new tomcat user <SpanGreen>username="sshb"</SpanGreen> <SpanGreen>password="123"</SpanGreen>, as manager & admin . paste code below
         </li>
         <LinuxHighlight bashCode={setup_user}></LinuxHighlight>
-        <li className="my-1"></li>
-        <li className="my-1"></li>
-        <li className="my-1"></li>
       </ul>
-      <DivDoubleBorder>config context.xml</DivDoubleBorder>
-      To remove the restriction for the Manager page, open its config file for editing:
+      <DivDoubleBorder>Remove restriction , Manager page</DivDoubleBorder>
+      To remove the restriction for the <SpanBlue>Manager</SpanBlue> page :
       <ul className="my-4 ml-8 list-decimal">
-        <li className="my-1">open CMD </li>
-        <li className="my-1"></li>
-        <li className="my-1"></li>
-        <li className="my-1"></li>
-        <li className="my-1"></li>
+        <li className="my-1">open its config file for editing</li>
+        <LinuxHighlight bashCode={context_restriction}></LinuxHighlight>
+        <li className="my-1">
+          Comment out the <SpanBlue>Valve</SpanBlue> tag definition, as shown:
+        </li>
+        <IMG img_name={tomcat_6}></IMG>
       </ul>
-      <DivDoubleBorder>config Host Manager</DivDoubleBorder>
+      <DivDoubleBorder>Remove restriction from Host Manager</DivDoubleBorder>
+      We also need to remove the restriction for the <SpanBlue>Host Manager</SpanBlue> page (As I did in prev section):
       <ul className="my-4 ml-8 list-decimal">
-        <li className="my-1">open CMD </li>
-        <li className="my-1"></li>
-        <li className="my-1"></li>
-        <li className="my-1"></li>
-        <li className="my-1"></li>
+        <li className="my-1">open its config file for editing</li>
+        <LinuxHighlight bashCode={host_restriction}></LinuxHighlight>
+        <li className="my-1">
+          Comment out the <SpanBlue>Valve</SpanBlue> tag definition, as shown: (Same as in previous section)
+        </li>
+        <IMG img_name={tomcat_6}></IMG>
       </ul>
     </MainChildArea>
   );
@@ -45,8 +46,14 @@ const O2_ConfigUsers = ({ anchor }: { anchor: string }) => {
 export default O2_ConfigUsers;
 
 const connect_to_srver = `ssh root@139.162.148.144`;
+
 const config_tomcat_1 = `sudo nano /opt/tomcat/conf/tomcat-users.xml`;
+
 const setup_user = `<role rolename="manager-gui"/>
 <role rolename="admin-gui"/>
 <role rolename="manager-script"/>
 <user username="sshb" password="123" roles="manager-gui,manager-script,admin-gui"/>`;
+
+const context_restriction = `sudo nano /opt/tomcat/webapps/manager/META-INF/context.xml`;
+
+const host_restriction = `sudo nano /opt/tomcat/webapps/host-manager/META-INF/context.xml`;
--- src/pages/DevOps/DevOpsComponents/LinodePkg/TomcatInstallSections/TomcatInstallMain.tsx
+++ src/pages/DevOps/DevOpsComponents/LinodePkg/TomcatInstallSections/TomcatInstallMain.tsx
@@ -2,19 +2,21 @@ import { useEffect, useRef, useState } from "react";
 import { ContentMenu } from "../../../../../components";
 import O1_InstallTomcat from "./O1_InstallTomcat";
 import O2_ConfigUsers from "./O2_ConfigUsers";
+import O3_ModifyPort from "./O3_ModifyPort";
 
 // ===========================================
 // ==     content menu (title name)         ==
 // ===========================================
 
-const O1_tomcat_install = "01_Install_tomcat_on_linux";
+const O1_tomcat_install = "O1_Install_tomcat_on_linux";
 const O2_config_users = "O2_config_users";
+const O3_modify_port = "O3_modify_port";
 
 // ===========================================
 // == Update anchorList with  content menu  ==
 // ===========================================
 
-const anchorList: string[] = [O1_tomcat_install, O2_config_users];
+const anchorList: string[] = [O1_tomcat_install, O2_config_users, O3_modify_port];
 
 // ============================================
 // ============================================
@@ -53,6 +55,8 @@ const TomcatInstallMain = () => {
       {/* End Contents */}
       <O1_InstallTomcat anchor={O1_tomcat_install}></O1_InstallTomcat>
       <O2_ConfigUsers anchor={O2_config_users}></O2_ConfigUsers>
+      <O3_ModifyPort anchor={O3_modify_port}></O3_ModifyPort>
+
       <div className="my-8 h-4">{/* {this div is only for dividing} */}</div>
     </section>
   );
