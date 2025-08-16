e7d7d3c676696c40f3734bc73b8f25c6c1e942cc
shabtay shalem <shabtay.shalem@gmail.com>
2025-08-17 00:41:14 +0300

    17-08-2025  0:41

--- src/pages/DevOps/DevOpsComponents/LinuxPkg/MySqlSections/O1_MySqlInstall.tsx
+++ src/pages/DevOps/DevOpsComponents/LinuxPkg/MySqlSections/O1_MySqlInstall.tsx
@@ -1,9 +1,30 @@
-import { MainChildArea } from "../../../../../components";
+import { GitHub, GitHubLiAnchor, MainChildArea } from "../../../../../components";
 
 const O1_MySqlInstall = ({ anchor }: { anchor: string }) => {
   return (
     <MainChildArea anchor={anchor}>
-      <div>To broswe to tomcat</div>
+      <GitHub>
+        <GitHubLiAnchor
+          description="mysql install on Linux"
+          gitLink="https://github.com/sshalem/DevOps/blob/main/Deploy_to_Tomcat/02_Install_tomcat_JDK_MySql_on_Linux.md#3_mysql_install"
+        ></GitHubLiAnchor>
+        <GitHubLiAnchor
+          description="create new user in MySql"
+          gitLink="https://github.com/sshalem/DevOps/blob/main/Deploy_to_Tomcat/01_Create_Linode_Linux_Server.md#6_1_new_user_in_mysql"
+        ></GitHubLiAnchor>
+        <GitHubLiAnchor
+          description="config workbench to remotely connect MySql on Linux server"
+          gitLink="https://github.com/sshalem/DevOps/blob/main/Deploy_to_Tomcat/01_Create_Linode_Linux_Server.md#6_2_remotely_connect_mysql_workbench"
+        ></GitHubLiAnchor>
+        <GitHubLiAnchor
+          description="config DBeaver to remotely connect MySql on Linux server"
+          gitLink="https://github.com/sshalem/DevOps/blob/main/Deploy_to_Tomcat/01_Create_Linode_Linux_Server.md#6_3_remotely_connect_with_dbeaver"
+        ></GitHubLiAnchor>
+        <GitHubLiAnchor
+          description="mysql install on Linux"
+          gitLink="https://github.com/sshalem/DevOps/blob/main/Deploy_to_Tomcat/02_Install_tomcat_JDK_MySql_on_Linux.md#3_mysql_install"
+        ></GitHubLiAnchor>
+      </GitHub>
     </MainChildArea>
   );
 };
