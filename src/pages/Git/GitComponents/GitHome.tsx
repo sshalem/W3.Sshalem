a1194fdc28e86052a918f95f48a200cd4f5ba7fc
shabtay shalem <shabtay.shalem@gmail.com>
2025-08-17 14:00:58 +0300

    17-08-2025 14:00

--- src/pages/Git/GitComponents/GitHome.tsx
+++ src/pages/Git/GitComponents/GitHome.tsx
@@ -1,5 +1,35 @@
+import { GitHub, GitHubLiAnchor } from "../../../components";
+import { BatchHighlight, SpanGreen } from "../../../components/Highlight";
+
 const GitHome = () => {
-  return <div className="bg-teal-500 p-4 font-mono text-4xl font-semibold tracking-wider text-white">GitHome Home ...</div>;
+  return (
+    <section>
+      <div className="bg-teal-500 p-4 font-mono text-4xl font-semibold tracking-wider text-white">GitHome Home ...</div>
+      <article className="my-8">
+        <SpanGreen>Git_all.bat</SpanGreen> script , that runs several commands
+        <BatchHighlight batchCode={git_commands}></BatchHighlight>
+      </article>
+      <GitHub>
+        <GitHubLiAnchor
+          description="1 - color batch script"
+          gitLink="https://github.com/sshalem/DevOps/tree/main/Batch%20script#2_color_batch_script"
+        ></GitHubLiAnchor>
+        <GitHubLiAnchor
+          description="2 - Git-all.batch script"
+          gitLink="https://github.com/sshalem/DevOps/tree/main/Batch%20script#3_git_batch_script"
+        ></GitHubLiAnchor>
+        <GitHubLiAnchor description="" gitLink=""></GitHubLiAnchor>
+        <GitHubLiAnchor description="" gitLink=""></GitHubLiAnchor>
+      </GitHub>
+    </section>
+  );
 };
 
 export default GitHome;
+
+const git_commands = `git pull --all
+git status
+git add --all
+git status
+git commit -m "message"
+git push --all`;
