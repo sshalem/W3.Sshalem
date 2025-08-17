b0ea6b7f0577158c38b845f4fd4fab762d1a8849
shabtay shalem <shabtay.shalem@gmail.com>
2025-08-17 10:59:37 +0300

    17-08-2025 10:59

--- src/pages/DevOps/DevOpsComponents/index.ts
+++ src/pages/DevOps/DevOpsComponents/index.ts
@@ -2,7 +2,7 @@ export { default as DevOpsHome } from "./DevOpsHome";
 export { default as Linux_Server } from "./LinuxPkg/Linux_Server";
 export { default as CreateServerMain } from "./LinuxPkg/CreateServerSections/CreateServerMain";
 export { default as ConnectToServerSshMain } from "./LinuxPkg/ConnectServerSshSections/ConnectToServerSshMain";
-export { default as ReactOnSpring } from "./ReactOnSpring/ReactOnSpring";
+export { default as ReactOnSpring } from "./ReactOnSpringPkg/ReactOnSpring";
 export { default as SetupServerMain } from "./LinuxPkg/SetupServerSections/SetupServerMain";
 export { default as JdkInstallMain } from "./LinuxPkg/JdkInstallSections/JdkInstallMain";
 export { default as TomcatInstallMain } from "./LinuxPkg/TomcatInstallSections/TomcatInstallMain";
