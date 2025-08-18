b8f5488ec61eddad9259b41397000608fddc109e
Shabtay Shalem <shabtay.shalem@asml.com>
2025-08-19 01:52:20 +0300

    08-19-2025  1:52

--- src/pages/ReactJS/ReactJSComponents/TypeScriptPkg/IntroSections/IntroMain.tsx
+++ src/pages/ReactJS/ReactJSComponents/TypeScriptPkg/IntroSections/IntroMain.tsx
@@ -3,6 +3,9 @@ import { ContentMenu } from "../../../../../components";
 import O1_Introduction from "./O1_Introduction";
 import O2_Variables from "./O2_Variables";
 import O3_UnknownNever from "./O3_UnknownNever";
+import O4_Type from "./O4_Type";
+import O6_Interface from "./O6_Interface";
+import O5_Union from "./O5_Union";
 
 // ===========================================
 // ==     content menu (title name)         ==
@@ -11,12 +14,15 @@ import O3_UnknownNever from "./O3_UnknownNever";
 const o1_introduction = "1. Introduction";
 const o2_variables = "2. Variables";
 const o3_unknown_never = "3. Unknown , never";
+const o4_type = "4. type";
+const o5_union = "5. union";
+const o6_interface = "6. interface ";
 
 // ===========================================
 // == Update anchorList with  content menu  ==
 // ===========================================
 
-const anchorList: string[] = [o1_introduction, o2_variables, o3_unknown_never];
+const anchorList: string[] = [o1_introduction, o2_variables, o3_unknown_never, o4_type, o5_union, o6_interface];
 
 // ============================================
 // ============================================
@@ -57,6 +63,9 @@ const IntroMain = () => {
       <O1_Introduction anchor={o1_introduction}></O1_Introduction>
       <O2_Variables anchor={o2_variables}></O2_Variables>
       <O3_UnknownNever anchor={o3_unknown_never}></O3_UnknownNever>
+      <O4_Type anchor={o4_type}></O4_Type>
+      <O5_Union anchor={o5_union}></O5_Union>
+      <O6_Interface anchor={o6_interface}></O6_Interface>
 
       <div className="my-8 h-4">{/* {this div is only for dividing} */}</div>
     </section>
