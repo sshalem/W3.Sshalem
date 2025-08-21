79fb51d43be81a045efd3c37f5708bda5e50bf6e
shabtay shalem <shabtay.shalem@gmail.com>
2025-08-21 23:42:22 +0300

    21-08-2025 23:42

--- src/components/ui/GitHub.tsx
+++ src/components/ui/GitHub.tsx
@@ -4,7 +4,7 @@ const GitHub = ({ children }: PropsWithChildren) => {
   return (
     <section className="my-10">
       <p className="mb-4 inline-block select-none rounded-md bg-blue-500 px-2 py-1 tracking-widest text-white">GitHub</p>
-      <ul className="mx-8 list-disc tracking-widest text-blue-500 hover:text-blue-700">{children}</ul>
+      <ul className="mx-8 list-disc tracking-wider text-blue-500 hover:text-blue-700">{children}</ul>
     </section>
   );
 };
