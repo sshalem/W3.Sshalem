d67bfba04492a0decc170ef2f2c4f3441649b390
shabtay shalem <shabtay.shalem@gmail.com>
2025-08-25 13:26:25 +0300

    25-08-2025 13:26

--- src/pages/MicroServices/MicroServiceComponents/KafkaPkg/IntroSections/O1_Introduction.tsx
+++ src/pages/MicroServices/MicroServiceComponents/KafkaPkg/IntroSections/O1_Introduction.tsx
@@ -3,6 +3,7 @@ import { DivDoubleBorder, SpanBlue, SpanGreen, SpanRed } from "../../../../../co
 import Li from "../../../../../components/ui/Li";
 import ULdisc from "../../../../../components/ui/ULdisc";
 import kafka_1 from "../../../../../assets/kafka_1.jpg";
+import ULDecimal from "../../../../../components/ui/ULDecimal";
 
 const O1_Introduction = ({ anchor }: { anchor: string }) => {
   return (
@@ -42,6 +43,10 @@ const O1_Introduction = ({ anchor }: { anchor: string }) => {
         <Li>
           See link from Baeldung <Anchor description="https://www.baeldung.com/apache-kafka" href="https://www.baeldung.com/apache-kafka"></Anchor>
         </Li>
+        <Li>
+          See link from geeksforgeeks{" "}
+          <Anchor description="what-is-a-kafka-broker" href="https://www.geeksforgeeks.org/cloud-computing/what-is-a-kafka-broker/"></Anchor>
+        </Li>
       </ULdisc>
       <DivDoubleBorder>Kafka Ecosystem</DivDoubleBorder>
       <div>
@@ -49,7 +54,62 @@ const O1_Introduction = ({ anchor }: { anchor: string }) => {
         <SpanBlue>Kafka</SpanBlue> consists of a set of <SpanGreen>brokers</SpanGreen>. A Cluster has minimum of 3<SpanGreen>brokers</SpanGreen>.
       </div>
       <IMG img_name={kafka_1}></IMG>
-      <DivDoubleBorder>Kafka Broker</DivDoubleBorder>
+      <DivDoubleBorder>1. Kafka Broker</DivDoubleBorder>
+      <section>
+        The Broker is the <SpanBlue>Kafka Server</SpanBlue>. A Kafka broker is like a single worker or machine in the Kafka system. Its main jobs are
+        <ULDecimal>
+          <Li>to receive new messages coming in safely</Li>
+          <Li> store those messages and provide the stored messages to any consumers that need them</Li>
+        </ULDecimal>
+        The broker acts as the middle person between producers sending messages and consumers receiving messages
+      </section>
+      <DivDoubleBorder>2. Cluster</DivDoubleBorder>
+      <section>
+        A Kafka cluster is a <strong>group of multiple Kafka brokers</strong> all working together. A Cluster has
+        <SpanGreen>minimum of 3 brokers</SpanGreen>. <br />
+        Having a cluster allows Kafka to handle very large amounts of data. If more data needs to be processed new brokers can easily be added to make
+        the cluster bigger. If less data needs processing, brokers can be removed to make the cluster smaller.
+      </section>
+      <DivDoubleBorder>3. Topic</DivDoubleBorder>
+      <section>
+        A topic is like a labelled box or category that related messages go into in Kafka. <br />
+        It is like a table in a DB or folder in a file system
+      </section>
+      <DivDoubleBorder>4. Partitions</DivDoubleBorder>
+      <section>
+        each topic is further divided into smaller partitions, which are like sub boxes inside the main topic box. Having these partitions allows
+        different parts of the big topic to be processed in parallel by multiple brokers at the same time. Partitions also make it easy to increase
+        processing power by simply adding more partitions as the amount of data grows.
+      </section>
+      <DivDoubleBorder>5. Replicas for reliability</DivDoubleBorder>
+      <section>
+        To ensure no data is lost if a broker fails, Kafka makes multiple copies or replicas of each partition across different brokers in the
+        cluster. So if one broker goes down the replicas on other brokers can still serve the messages, providing reliability and preventing data
+        loss.
+      </section>
+      <DivDoubleBorder>5. Offsets</DivDoubleBorder>
+      <section>
+        Offsets is a sequence of id's given to messages as they arrive at a partition. Once the offset is assigned it will never be changed. The first
+        message gets offset zero. the next gets offset of one, and so on.
+      </section>
+      <DivDoubleBorder>6. Messages</DivDoubleBorder>
+      <section>These messages can contain any type of data like logs, events, records or other information from the producer</section>
+      <DivDoubleBorder>7. Producer</DivDoubleBorder>
+      <section>
+        Is Application that send messages only to <SpanBlue>Kafka server</SpanBlue>
+      </section>
+      <DivDoubleBorder>8. Consumer</DivDoubleBorder>
+      <section>
+        Is Application that reads messages from <SpanBlue>Kafka server</SpanBlue>
+      </section>
+      <DivDoubleBorder>8. Consumer Groups</DivDoubleBorder>
+      <section>A Consumer group contains one or more consumers working together to process the messages.</section>
+      <DivDoubleBorder>10. Fault Tolerance</DivDoubleBorder>
+      <section>
+        Kafka provides the <SpanBlue>fault tolerance</SpanBlue> by making the multiple copies ( replicas ) of the data. Each partitions data is copied
+        across different brokers. If one broker fails the another broker with the replica can easily take over as the leader ensuring operations keep
+        running and the data remains available.
+      </section>
     </MainChildArea>
   );
 };
