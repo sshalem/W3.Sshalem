05dc2222df9465b0647c378251bde16fedbc1067
shabtay shalem <shabtay.shalem@gmail.com>
2025-08-25 11:27:57 +0300

    25-08-2025 11:27

--- src/pages/MicroServices/MicroServiceComponents/KafkaPkg/Kafka.tsx
+++ src/pages/MicroServices/MicroServiceComponents/KafkaPkg/Kafka.tsx
@@ -1,9 +1,33 @@
 import { Outlet, useLocation } from "react-router-dom";
+import { Anchor } from "../../../../components";
 
 const Kafka = () => {
   let location = useLocation();
 
-  return <section>{location.pathname === "/microservices/kafka" ? <div className="text-3xl">Kafka page ...</div> : <Outlet />}</section>;
+  return (
+    <section>
+      {location.pathname === "/microservices/kafka" ? (
+        <section className="my-8 text-2xl">
+          <div>
+            From course on udemy{" "}
+            <Anchor
+              description="apache kafka - Ramesh Fadatare "
+              href="https://www.udemy.com/course/building-microservices-with-spring-boot-and-spring-cloud/learn/lecture/35997454#overview"
+            ></Anchor>{" "}
+          </div>
+          <div>
+            part of course{" "}
+            <Anchor
+              description="Building Microservices with Spring Boot & Spring Cloud"
+              href="https://www.udemy.com/course/building-microservices-with-spring-boot-and-spring-cloud/?couponCode=25BBPMXINACTIVE"
+            ></Anchor>
+          </div>
+        </section>
+      ) : (
+        <Outlet />
+      )}
+    </section>
+  );
 };
 
 export default Kafka;
--- src/pages/MicroServices/MicroServiceComponents/RabbitMQPkg/RabbitMQ.tsx
+++ src/pages/MicroServices/MicroServiceComponents/RabbitMQPkg/RabbitMQ.tsx
@@ -1,9 +1,33 @@
 import { Outlet, useLocation } from "react-router-dom";
+import { Anchor } from "../../../../components";
 
 const RabbitMQ = () => {
   let location = useLocation();
 
-  return <section>{location.pathname === "/microservices/rabbitmq" ? <div className="text-3xl">RabbitMQ page ...</div> : <Outlet />}</section>;
+  return (
+    <section>
+      {location.pathname === "/microservices/rabbitmq" ? (
+        <section className="my-8">
+          <div className="text-2xl">
+            From course on udemy{" "}
+            <Anchor
+              description="RabbitMQ - Ramesh Fadatare "
+              href="https://www.udemy.com/course/building-microservices-with-spring-boot-and-spring-cloud/learn/lecture/35929576#overview"
+            ></Anchor>{" "}
+          </div>
+          <div className="text-2xl">
+            part of course{" "}
+            <Anchor
+              description="Building Microservices with Spring Boot & Spring Cloud"
+              href="https://www.udemy.com/course/building-microservices-with-spring-boot-and-spring-cloud/?couponCode=25BBPMXINACTIVE"
+            ></Anchor>
+          </div>
+        </section>
+      ) : (
+        <Outlet />
+      )}
+    </section>
+  );
 };
 
 export default RabbitMQ;
