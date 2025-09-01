6a05d12b9b52f7c70a8e12f54e9c0b3f51dd58eb
Shabtay Shalem <shabtay.shalem@asml.com>
2025-09-01 13:05:03 +0300

    09-01-2025 13:05

--- src/pages/Spring/SpringComponents/ApplicationPropsPkg/DBeaverSections/DBeaverPostgresql.tsx
+++ src/pages/Spring/SpringComponents/ApplicationPropsPkg/DBeaverSections/DBeaverPostgresql.tsx
@@ -2,15 +2,17 @@ import { FaDiamond } from "react-icons/fa6";
 import dbeaver_postgres_1 from "../../../../../assets/dbeaver_postgres_1.jpg";
 import dbeaver_postgres_2 from "../../../../../assets/dbeaver_postgres_2.jpg";
 import dbeaver_postgres_3 from "../../../../../assets/dbeaver_postgres_3.jpg";
-import { SpanBlue } from "../../../../../components/Highlight";
+import { SpanBlue, SpanRed, SpanTeal } from "../../../../../components/Highlight";
 import { IMG, MainChildArea } from "../../../../../components";
 
 const DBeaverPostgresql = ({ anchor }: { anchor: string }) => {
   return (
     <MainChildArea anchor={anchor}>
+      <div>In order to connect a single DB with DBeaver follow these steps.</div>
       <ul className="mx-8">
+        <li className="my-1 list-disc">I have multiple DB's in PGadmin.</li>
         <li className="my-1 list-disc">
-          <SpanBlue>cms</SpanBlue> is my Database in PgAdmin
+          I will connect <SpanRed>only</SpanRed> <SpanBlue>cms</SpanBlue> is my Database in PgAdmin , since I don't all DB's to be shown in DBeaver.
         </li>
         <li className="my-1 list-disc">
           Lets see how I connect DBeaver with <SpanBlue>cms</SpanBlue> DB on Postgresql
@@ -26,8 +28,8 @@ const DBeaverPostgresql = ({ anchor }: { anchor: string }) => {
             <FaDiamond className="mr-2 self-center text-[0.6rem]" /> <SpanBlue>cms</SpanBlue> - thats the name of DB in PgAdmin
           </div>
           <div className="my-1 ml-8 flex">
-            <FaDiamond className="mr-2 self-center text-[0.6rem]" /> Note - I didnt select Show all databases (because it will be to much DB's ,
-            unless I want them all)
+            <FaDiamond className="mr-2 self-center text-[0.6rem]" /> <SpanRed>Note</SpanRed> - I didnt select <SpanTeal>Show all databases</SpanTeal>{" "}
+            (because it will be to much DB's , unless I want them all)
           </div>
           <div className="my-1 ml-8 flex">
             <FaDiamond className="mr-2 self-center text-[0.6rem]" /> user and password : both are <SpanBlue>postgres</SpanBlue> (as I define when I
