4c5956ae6e823de27b2d7d5a720260281ff48ad2
shabtay shalem <shabtay.shalem@gmail.com>
2025-09-07 01:24:50 +0300

    07-09-2025  1:24

--- src/pages/Spring/SpringComponents/InterviewQuestionsPkg/JavaInUseSections/O1_SpringBootInterview.tsx
+++ src/pages/Spring/SpringComponents/InterviewQuestionsPkg/JavaInUseSections/O1_SpringBootInterview.tsx
@@ -3,33 +3,100 @@ import { Anchor, MainChildArea } from "../../../../../components";
 const O1_SpringBootInterview = ({ anchor }: { anchor: string }) => {
   return (
     <MainChildArea anchor={anchor}>
-      <div className="my-4">
-        Here is a list of question by <strong>JAVA In Use</strong> web site
-      </div>
-      <p>
-        <Anchor description="Spring Boot Interview Question 2025" href="https://www.javainuse.com/spring/SpringBootInterviewQuestions"></Anchor>
-      </p>
-      <p>
-        <Anchor description="Spring AOP Question" href="https://www.javainuse.com/spring/spring-AOP-interview-quesions"></Anchor>
-      </p>
-      <p>
-        <Anchor description="Spring Batch Question" href="https://www.javainuse.com/spring/sprbatch_interview"></Anchor>
-      </p>
-      <p>
-        <Anchor description="JAVA 8 questions" href="https://www.javainuse.com/java/java8_intvw"></Anchor>
-      </p>
-      <p>
-        <Anchor description="Spring Cloud questions" href="https://www.javainuse.com/spring/spring-cloud-interview-questions"></Anchor>
-      </p>
-      <p>
-        <Anchor description="Microservices Interview Quesions" href="https://www.javainuse.com/spring/microservices-interview-quesions"></Anchor>
-      </p>
-      <p>
-        <Anchor description="JAVA HashMap vs Cuncurrent Map" href="https://www.javainuse.com/java/java_map_intvw"></Anchor>
-      </p>
-      <p>
-        <Anchor description="Maven interview questions" href="https://www.javainuse.com/misc/maven-interview-questions"></Anchor>
-      </p>
+      <article className="mb-10">
+        <div className="my-4">
+          Here is a list of question by <strong>JAVA In Use</strong> web site
+        </div>
+        <p>
+          <Anchor description="1. Spring Boot Interview Question 2025" href="https://www.javainuse.com/spring/SpringBootInterviewQuestions"></Anchor>
+        </p>
+        <p>
+          <Anchor description="2. Spring AOP Question" href="https://www.javainuse.com/spring/spring-AOP-interview-quesions"></Anchor>
+        </p>
+        <p>
+          <Anchor description="3. Spring Batch Question" href="https://www.javainuse.com/spring/sprbatch_interview"></Anchor>
+        </p>
+
+        <p>
+          <Anchor description="4. Spring Cloud questions" href="https://www.javainuse.com/spring/spring-cloud-interview-questions"></Anchor>
+        </p>
+        <p>
+          <Anchor description="5. Microservices Interview Quesions" href="https://www.javainuse.com/spring/microservices-interview-quesions"></Anchor>
+        </p>
+
+        <p>
+          <Anchor description="6. Maven interview questions" href="https://www.javainuse.com/misc/maven-interview-questions"></Anchor>
+        </p>
+        <p>
+          <Anchor description="7. Fullstack interview questions" href="https://www.javainuse.com/fullstack/intvw"></Anchor>
+        </p>
+        <p>
+          <Anchor
+            description="8. Continuous Integration interview questions"
+            href="https://www.javainuse.com/misc/continuous-integration-interview-questions"
+          ></Anchor>
+        </p>
+        <p>
+          <Anchor description="9. SWAGGER questions" href="https://www.javainuse.com/misc/swag"></Anchor>
+        </p>
+        <p>
+          <Anchor description="" href=""></Anchor>
+        </p>
+        <p>
+          <Anchor description="" href=""></Anchor>
+        </p>
+      </article>
+      {/*  */}
+      <hr />
+      {/*  */}
+      <article>
+        <div className="my-4">
+          JAVA Interview question from <strong>JAVA In Use</strong> web site
+        </div>
+        <p>
+          <Anchor description="1. JAVA 8 questions" href="https://www.javainuse.com/java/java8_intvw"></Anchor>
+        </p>
+        <p>
+          <Anchor description="2. JAVA HashMap vs Cuncurrent Map" href="https://www.javainuse.com/java/java_map_intvw"></Anchor>
+        </p>
+        <p>
+          <Anchor description="3. JAVA Regex questions" href="https://www.javainuse.com/misc/regex-interview-questions"></Anchor>
+        </p>
+        <p>
+          <Anchor description="4. JPA interview questions" href="https://www.javainuse.com/misc/jpa-interview-questions"></Anchor>
+        </p>
+        <p>
+          <Anchor description="5. ENUM questions" href="https://www.javainuse.com/misc/enum-interview-questions"></Anchor>
+        </p>
+        <p>
+          <Anchor description="6. REDIS questions" href="https://www.javainuse.com/misc/redis-interview-questions"></Anchor>
+        </p>
+        <p>
+          <Anchor description="7. Java Inner Class Interview Questions" href="https://www.javainuse.com/misc/inner-interview-questions"></Anchor>
+        </p>
+        <p>
+          <Anchor
+            description="8. Java Immutable Class frequently asked interview questions"
+            href="https://www.javainuse.com/misc/immutable-interview-questions"
+          ></Anchor>
+        </p>
+        <p>
+          <Anchor
+            description="9. Java Serialization frequently asked interview questions"
+            href="https://www.javainuse.com/misc/serial-interview-questions"
+          ></Anchor>
+        </p>
+        <p>
+          <Anchor
+            description="10. Java 11 frequently asked interview questions"
+            href="https://www.javainuse.com/misc/java11-interview-questions"
+          ></Anchor>
+        </p>
+
+        <p>
+          <Anchor description="" href=""></Anchor>
+        </p>
+      </article>
     </MainChildArea>
   );
 };
