37f03cf54c23f3881034ef85e1192ec9987c45c0
shabtay shalem <shabtay.shalem@gmail.com>
2025-09-16 21:43:52 +0300

    16-09-2025 21:43

--- src/pages/DevOps/DevOpsComponents/index.ts
+++ src/pages/DevOps/DevOpsComponents/index.ts
@@ -1,17 +1,17 @@
 export { default as DevOpsHome } from "./DevOpsHome";
-export { default as Linux_Server } from "./LinuxPkg/Linux_Server";
-export { default as CreateServerMain } from "./LinuxPkg/CreateServerSections/CreateServerMain";
-export { default as ConnectToServerSshMain } from "./LinuxPkg/ConnectServerSshSections/ConnectToServerSshMain";
-export { default as ReactOnSpringMain } from "./ReactOnSpringPkg/ReactOnSpringMain";
-export { default as SetupServerMain } from "./LinuxPkg/SetupServerSections/SetupServerMain";
-export { default as JdkInstallMain } from "./LinuxPkg/JdkInstallSections/JdkInstallMain";
-export { default as TomcatInstallMain } from "./LinuxPkg/TomcatInstallSections/TomcatInstallMain";
-export { default as MySqlnstallMain } from "./LinuxPkg/MySqlSections/MySqlnstallMain";
-export { default as JenkinsMain } from "./LinuxPkg/JenkinsSections/JenkinsMain";
-export { default as FileZillaMain } from "./LinuxPkg/ConnectFileZillaSections/FileZillaMain";
+export { default as Linux_Server } from "./D_LinuxPkg/Linux_Server";
+export { default as CreateServerMain } from "./D_LinuxPkg/CreateServerSections/CreateServerMain";
+export { default as ConnectToServerSshMain } from "./D_LinuxPkg/ConnectServerSshSections/ConnectToServerSshMain";
+export { default as ReactOnSpringMain } from "./D_ReactOnSpringPkg/ReactOnSpringMain";
+export { default as SetupServerMain } from "./D_LinuxPkg/SetupServerSections/SetupServerMain";
+export { default as JdkInstallMain } from "./D_LinuxPkg/JdkInstallSections/JdkInstallMain";
+export { default as TomcatInstallMain } from "./D_LinuxPkg/TomcatInstallSections/TomcatInstallMain";
+export { default as MySqlnstallMain } from "./D_LinuxPkg/MySqlSections/MySqlnstallMain";
+export { default as JenkinsMain } from "./D_LinuxPkg/JenkinsSections/JenkinsMain";
+export { default as FileZillaMain } from "./D_LinuxPkg/ConnectFileZillaSections/FileZillaMain";
 
-export { default as DeployNetlifyMain } from "./DeployNetlifyPkg/DeployNetlifyMain";
-export { default as DeployRenderMain } from "./DeployRenderPkg/DeployRenderMain";
+export { default as DeployNetlifyMain } from "./D_DeployNetlifyPkg/DeployNetlifyMain";
+export { default as DeployRenderMain } from "./D_DeployRenderPkg/DeployRenderMain";
 
-export { default as Tomcat } from "./TomcatPkg/Tomcat";
-export { default as ReactOnTomcatMain } from "./TomcatPkg/DeployReactOnTomcat/ReactOnTomcatMain";
+export { default as Tomcat } from "./D_TomcatPkg/Tomcat";
+export { default as ReactOnTomcatMain } from "./D_TomcatPkg/DeployReactOnTomcat/ReactOnTomcatMain";
