494e77d5e244453ec65175d5798a1fc165d39a9e
shabtay shalem <shabtay.shalem@gmail.com>
2025-09-30 16:48:40 +0300

    30-09-2025 16:48

--- src/pages/Spring/SpringComponents/S7_SecurityPkg/C_JwtImplSections/O12_SecurityConfig.tsx
+++ src/pages/Spring/SpringComponents/S7_SecurityPkg/C_JwtImplSections/O12_SecurityConfig.tsx
@@ -1,8 +1,16 @@
 import { Anchor, MainChildArea } from "../../../../../components";
 import ULdisc from "../../../../../components/ui/ULdisc";
 import Li from "../../../../../components/ui/Li";
+import SpanGrey from "../../../../../components/Highlight/SpanGrey";
+import { useMemo } from "react";
+import { JavaHighlight } from "../../../../../components/Highlight";
 
 const O12_SecurityConfig = ({ anchor }: { anchor: string }) => {
+  // 1. I Memoize Syntax Highlighter , for better perfromance
+  // 2. This fixes , the DropDown issue I have , when I click the Show/Hide content
+  const renderSecurityConfig = useMemo(() => <JavaHighlight javaCode={jwt_security_config} />, [jwt_security_config]);
+  const renderSecurityConfigDsl = useMemo(() => <JavaHighlight javaCode={jwt_security_config_dsl_lambda} />, [jwt_security_config_dsl_lambda]);
+
   return (
     <MainChildArea anchor={anchor}>
       <section className="my-8">
@@ -17,17 +25,34 @@ const O12_SecurityConfig = ({ anchor }: { anchor: string }) => {
           </Li>
         </ULdisc>
       </section>
-      <hr />
 
       <section className="my-8">
-        <p className="my-4 text-lg font-semibold">🧩 Dependencies + POM file</p>
         <article>
           <ULdisc>
-            <Li></Li>
+            <Li>
+              Need to Disable <SpanGrey>csrf()</SpanGrey>
+            </Li>
+            <Li>
+              Most of the time I also add <SpanGrey>cors()</SpanGrey> , in this example I didn't do that.
+            </Li>
           </ULdisc>
         </article>
       </section>
       <hr />
+
+      <section className="my-8">
+        <p className="font-semibold">
+          🔑 <SpanGrey>SecurityConfig</SpanGrey> code
+        </p>
+        {renderSecurityConfig}
+      </section>
+
+      <section className="my-8">
+        <p className="font-semibold">
+          🔑 <SpanGrey>SecurityConfig with DSL lambda</SpanGrey> code
+        </p>
+        {renderSecurityConfigDsl}
+      </section>
 
       <article>
         {/* <ULdisc>
@@ -42,3 +67,150 @@ const O12_SecurityConfig = ({ anchor }: { anchor: string }) => {
 };
 
 export default O12_SecurityConfig;
+
+const jwt_security_config = `package com.O2.config;
+
+import com.O2.jwt.JwtAuthenticationEntryPoint;
+import com.O2.jwt.JwtAuthenticationFilter;
+import com.O2.jwt.JwtUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
+	@Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	@Autowired
+	private JwtUserDetailsService jwtUserDetailsService;
+
+	@Bean
+	PasswordEncoder passEncode() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passEncode());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+		.and()
+			.authorizeRequests()
+			.antMatchers("/*", "/css/*", "/js/*").permitAll()
+			.antMatchers("/h2/**/**").permitAll() // Should not be in Production!
+			.antMatchers("/auth/**").permitAll()
+			.antMatchers("/api/users/**").hasAnyRole("SUPER-ADMIN", "ADMIN")
+			.antMatchers("/api/roles/**").hasAnyRole("SUPER-ADMIN", "ADMIN")
+			.anyRequest()
+			.authenticated()
+		.and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        // fix H2 database console:
+		// Refused to display ' in a frame because it set 'X-Frame-Options' to 'deny'
+		http
+			.headers()
+			.frameOptions()
+			.sameOrigin() // H2 Console Needs this setting
+			.cacheControl(); // disable caching
+	}
+}`;
+
+const jwt_security_config_dsl_lambda = `package com.O2.config;
+
+import com.O2.jwt.JwtAuthenticationEntryPoint;
+import com.O2.jwt.JwtAuthenticationFilter;
+import com.O2.jwt.JwtUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+	@Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	
+	@Autowired
+	private JwtUserDetailsService jwtUserDetailsService;
+	
+	@Bean
+	PasswordEncoder passEncode() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passEncode());
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+        http
+        	.csrf(csrf -> csrf.disable())
+        	.exceptionHandling(handling -> handling.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+            .authorizeRequests(requests -> requests
+            	.antMatchers("/*", "/css/*", "/js/*").permitAll()
+                .antMatchers("/h2/**/**").permitAll() // Should not be in Production!
+                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/api/users/**").hasAnyRole("SUPER-ADMIN", "ADMIN")
+                .antMatchers("/api/roles/**").hasAnyRole("SUPER-ADMIN", "ADMIN")
+                .anyRequest()
+                .authenticated())
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		
+		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        // fix H2 database console: 
+        // Refused to display ' in a frame because it set 'X-Frame-Options' to 'deny'
+        http.headers(headers -> headers
+        				.frameOptions()
+                        .sameOrigin() // H2 Console Needs this setting
+                        .cacheControl()); // disable caching
+	}
+}`;
