7d59b01ab92d0d5f3a001f70b8b73a1a4b68924e
Shabtay Shalem <shabtay.shalem@asml.com>
2025-10-03 13:47:22 +0300

    10-03-2025 13:47

--- src/pages/Spring/SpringComponents/S7_SecurityPkg/C_JwtImplSections/O0_IntroJwtImpl.tsx
+++ src/pages/Spring/SpringComponents/S7_SecurityPkg/C_JwtImplSections/O0_IntroJwtImpl.tsx
@@ -1,5 +1,6 @@
 import { Anchor, MainChildArea } from "../../../../../components";
 import { SpanBlue, SpanRed } from "../../../../../components/Highlight";
+import SpanGrey from "../../../../../components/Highlight/SpanGrey";
 import Li from "../../../../../components/ui/Li";
 import ULdisc from "../../../../../components/ui/ULdisc";
 
@@ -11,14 +12,30 @@ const O0_IntroJwtImpl = ({ anchor }: { anchor: string }) => {
         In this implementations:
         <ULdisc>
           <Li>
-            I use Spring boot version <SpanBlue>2.6.11</SpanBlue>. <br />
-            <SpanRed>Note</SpanRed> : from Spring boot 2.7 , the SecurityConfig is changed (See in the code )
+            I use Spring boot version <SpanGrey>2.6.11</SpanGrey>. <br />
+          </Li>
+
+          <Li>
+            I use <SpanGrey>jdk-11</SpanGrey> version
           </Li>
           <Li>
-            I use <SpanBlue>jdk-11</SpanBlue> version
+            I use <SpanGrey>Postman</SpanGrey> for testing
           </Li>
           <Li>
-            I use <SpanBlue>Postman</SpanBlue> for testing
+            <SpanRed>Important Note</SpanRed> :
+            <ULdisc>
+              <Li>from Spring boot 2.7 , the SecurityConfig is changed (See in the code )</Li>
+              <Li>
+                link from bezkoder :{" "}
+                <Anchor
+                  description="https://www.bezkoder.com/websecurityconfigureradapter-deprecated-spring-boot/"
+                  href="https://www.bezkoder.com/websecurityconfigureradapter-deprecated-spring-boot/"
+                ></Anchor>{" "}
+                <br />
+                From Spring Boot 2.7, <SpanGrey>WebSecurityConfigurerAdapter</SpanGrey>
+                is deprecated. (See Link JWT Spring v3.5.6)
+              </Li>
+            </ULdisc>
           </Li>
           <Li>
             🔑 GitHub project link ⇨{" "}
