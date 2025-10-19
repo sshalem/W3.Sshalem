4c6a745ec36f725208e2091151c033509622b3b4
shabtay shalem <shabtay.shalem@gmail.com>
2025-10-19 15:29:04 +0300

    19-10-2025 15:29

--- src/pages/Spring/SpringComponents/S3_LoggingPkg/LoggingProperties/LoggingPropsIntro.tsx
+++ src/pages/Spring/SpringComponents/S3_LoggingPkg/LoggingProperties/LoggingPropsIntro.tsx
@@ -1,12 +1,45 @@
-import { MainChildArea } from "../../../../../components";
+import { Li, MainChildArea, ULdisc } from "../../../../../components";
+import { JavaHighlight, SpanGrey } from "../../../../../components/Highlight";
 
 const LoggingPropsIntro = ({ anchor }: { anchor: string }) => {
   return (
     <MainChildArea anchor={anchor}>
       In this section , I will show , how I can log, every class from any package I want on Spring boot app, including packages/classes from different
       packages that I import.
+      <ULdisc>
+        <Li>
+          💡 Global logging level - <SpanGrey>logging.level.root=INFO</SpanGrey>
+        </Li>
+        <Li>
+          💡 Log only a specific package in detail - <SpanGrey>logging.level.com.example.myapp.service=DEBUG</SpanGrey>
+        </Li>
+        <Li>
+          💡 Another example to log only a specific package in detail -
+          <SpanGrey>logging.level.org.springframework.security.web.FilterChainProxy=TRACE</SpanGrey>
+        </Li>
+      </ULdisc>
+      In the Example Below Spring Boot tells to
+      <ULdisc>
+        <Li>
+          log everything in <SpanGrey>com.example.myapp.service</SpanGrey> at <SpanGrey>DEBUG</SpanGrey> level
+        </Li>
+        <Li>
+          while keeping the rest at <SpanGrey>INFO</SpanGrey>
+        </Li>
+      </ULdisc>
+      <JavaHighlight javaCode={logging}></JavaHighlight>
     </MainChildArea>
   );
 };
 
 export default LoggingPropsIntro;
+
+const logging = `# Global logging level
+logging.level.root=INFO
+
+# Log only a specific package in detail
+logging.level.com.example.myapp.service=DEBUG
+
+# You can also control other packages (e.g., Hibernate)
+logging.level.org.hibernate.SQL=DEBUG
+logging.level.org.hibernate.type.descriptor.sql=TRACE`;
--- src/pages/Spring/SpringComponents/S4_JpaPkg/J_JpaLoggingSections/O1_JpaLogging.tsx
+++ src/pages/Spring/SpringComponents/S4_JpaPkg/J_JpaLoggingSections/O1_JpaLogging.tsx
@@ -1,4 +1,4 @@
-import { MainChildArea } from "../../../../../components";
+import { Anchor, Li, MainChildArea, ULdisc } from "../../../../../components";
 import { ApplicationPropertiesHighlight } from "../../../../../components/Highlight";
 
 const O1_JpaLogging = ({ anchor }: { anchor: string }) => {
@@ -6,6 +6,11 @@ const O1_JpaLogging = ({ anchor }: { anchor: string }) => {
     <MainChildArea anchor={anchor}>
       <section className="my-8">
         <p className="my-4 text-lg font-semibold">🔑 JPA logging</p>
+        <ULdisc>
+          <Li>
+            To log sepcific packages see section - <Anchor description="logging-properties" href="/spring/logging/logging-props"></Anchor>{" "}
+          </Li>
+        </ULdisc>
 
         <ApplicationPropertiesHighlight propertiesCode={jpa_logging}></ApplicationPropertiesHighlight>
       </section>
