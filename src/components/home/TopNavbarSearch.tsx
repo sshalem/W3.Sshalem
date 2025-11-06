1b15138fed12dd20e8fbb30337aafad630096069
Shabtay Shalem <shabtay.shalem@asml.com>
2025-11-06 23:12:19 +0200

    11-06-2025 23:12

--- src/components/home/TopNavbarSearch.tsx
+++ src/components/home/TopNavbarSearch.tsx
@@ -1,16 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
 const TopNavbarSearch = () => {
   const [pages, setPages] = useState<any[]>([]);
   const [query, setQuery] = useState("");
   const [results, setResults] = useState<any[]>([]);
-
-  useEffect(() => {
-    fetch("/searchIndex.json")
-      .then((res) => res.json())
-      .then(setPages);
-  }, []);
+  const [width, setWidth] = useState<number>();
+  const ulRef = useRef<HTMLUListElement | null>(null);
+  const liRefs = useRef<(HTMLLIElement | null)[]>([]);
 
   const handleClearSearch = () => {
     setResults([]);
@@ -30,6 +27,19 @@ const TopNavbarSearch = () => {
     setResults(res);
   };
 
+  useEffect(() => {
+    if (ulRef.current !== null) {
+      const newWidth = ulRef.current.clientWidth + 220;
+      setWidth(newWidth);
+    }
+  }, [query]);
+
+  useEffect(() => {
+    fetch("/searchIndex.json")
+      .then((res) => res.json())
+      .then(setPages);
+  }, []);
+
   // I want to make the width of drop down of the search functionaly ,
   // to dynamic, meaning to change according the length of the String
   return (
@@ -42,10 +52,16 @@ const TopNavbarSearch = () => {
         value={query}
         onChange={(e) => handleSearch(e.target.value)}
       />
-      <ul className="absolute top-[78px] w-auto max-w-fit bg-zinc-200 font-mono text-sm text-black">
+      <ul className="absolute top-[78px] bg-zinc-200 font-mono text-sm text-black" style={{ width: `${width}px` }} ref={ulRef}>
         {results.map((res, index) => {
           return (
-            <li key={index} className="hover:w-auto hover:bg-blue-200 hover:text-black">
+            <li
+              key={index}
+              className="hover:bg-blue-200 hover:text-black"
+              ref={(el) => {
+                liRefs.current[index] = el;
+              }}
+            >
               <Link to={res.url} onClick={handleClearSearch}>
                 <span className="ml-4">{res.component}</span>
               </Link>
