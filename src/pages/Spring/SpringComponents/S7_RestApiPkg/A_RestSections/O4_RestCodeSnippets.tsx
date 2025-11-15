89c0a766916d5eb57303da94f2a9206949ffc898
shabtay shalem <shabtay.shalem@gmail.com>
2025-11-15 23:55:59 +0200

    15-11-2025 23:55

--- src/pages/Spring/SpringComponents/S7_RestApiPkg/A_RestSections/O4_RestCodeSnippets.tsx
+++ src/pages/Spring/SpringComponents/S7_RestApiPkg/A_RestSections/O4_RestCodeSnippets.tsx
@@ -3,7 +3,7 @@
 Rest Code Snippets --> (SPRING)(rest)(rest-api)
 */
 
-import { Li, MainChildArea, ULdisc } from "../../../../../components";
+import { Li, MainChildArea, ULDecimal, ULdisc } from "../../../../../components";
 import { JavaHighlight, SpanGreen, SpanGrey } from "../../../../../components/Highlight";
 
 const O4_RestCodeSnippets = ({ anchor }: { anchor: string }) => {
@@ -69,8 +69,9 @@ const O4_RestCodeSnippets = ({ anchor }: { anchor: string }) => {
           <Li>Backend read the QueryParams from url</Li>
         </ULdisc>
         <JavaHighlight javaCode={request_param_1}></JavaHighlight>
+        <article className="my-8 text-2xl font-semibold">⭐ @RequestParam optional parameters</article>
         <SpanGrey>@RequestParam</SpanGrey> also has optional parameters :
-        <ULdisc>
+        <ULDecimal>
           <Li>
             <SpanGrey>required</SpanGrey> - can be true/false (true = mandatory, false = NOT mandatory)
           </Li>
@@ -87,8 +88,41 @@ const O4_RestCodeSnippets = ({ anchor }: { anchor: string }) => {
               <Li>shalem</Li> because there is no value assign , thus default value is shalem
             </ULdisc>
           </Li>
+        </ULDecimal>
+        <JavaHighlight javaCode={request_param_required_default}></JavaHighlight>
+        <article className="my-8 text-2xl font-semibold">⭐ @RequestParam multiple values for the same key</article>
+        <div>I can also multiple values for the same key:</div>
+        <ULdisc>
+          <Li>
+            FrontEnd sends <SpanGrey>GET</SpanGrey> request as follows , same <strong>tag</strong> key
+            <SpanGrey>http://localhost:8080/api//get/listParam?tag=first tag&tag=secncond tag&tag=third tag</SpanGrey>
+          </Li>
+          <Li>
+            The Query Params from the url are seperated by <SpanGrey>&</SpanGrey> sign.
+          </Li>
+          <Li>
+            Backend read the QueryParams , <SpanGrey>tag</SpanGrey>
+          </Li>
+          <Li>
+            <SpanGrey>tag</SpanGrey> is the key,and I have multiple tags
+          </Li>
         </ULdisc>
-        <JavaHighlight javaCode={request_param_required_default}></JavaHighlight>I can also multiple values for the same key:
+        <JavaHighlight javaCode={request_param_list_parameters_same_key}></JavaHighlight>
+        {/*  */}
+        <article className="my-8 text-2xl font-semibold">⭐ @RequestParam Map All Parameters</article>
+        <div>I can also map all params regardless of the key :</div>
+        <ULdisc>
+          <Li>
+            FrontEnd sends <SpanGrey>GET</SpanGrey> request as with all kind of keys
+            <SpanGrey>/get/mapParam?firstName=shabtay&lastName=shalem&gender=male&age=50</SpanGrey>
+          </Li>
+          <Li>
+            The Query Params from the url are seperated by <SpanGrey>&</SpanGrey> sign.
+          </Li>
+          <Li>Backend read the QueryParams , and stored them in a Map</Li>
+          <Li>I have 4 different keys in the url params</Li>
+        </ULdisc>
+        <JavaHighlight javaCode={request_param_map_params_multuple_keys}></JavaHighlight>
       </section>
     </MainChildArea>
   );
@@ -108,20 +142,30 @@ public Person getPerson(@PathVariable String firstName, @PathVariable long id) {
 }`;
 
 const request_param_1 = `@GetMapping(path = "/getAll")
-public ResponseEntity<List<Person>> getAllPersons(@RequestParam long id, @RequestParam String firsName) {
-    return ResponseEntity.ok().body(personServiceImpl.getAllPersons());
+public ResponseEntity<?> getAllPersons(
+    @RequestParam long id, 
+    @RequestParam String firsName) {
+    return null;
 }`;
 
 const request_param_required_default = `	@GetMapping(path = "/getAll/required")
-	public ResponseEntity<List<Person>> getAllPersonsRequired(
+	public ResponseEntity<?> getAllPersonsRequired(
 			@RequestParam(required = false) Long id,
 			@RequestParam(required = true, defaultValue = "shalem") String firsName) {
 		System.out.println(id + " " + firsName) ;		
-		return ResponseEntity.ok().body(personServiceImpl.getAllPersons());
+		return null;
 	}`;
 
-// const request_param_list_parameters = `
-// @GetMapping("/items")
-// public String getItems(@RequestParam List<String> ids) {
-//     return "Items: " + String.join(", ", ids);
-// }`;
+const request_param_list_parameters_same_key = `@GetMapping(path = "/get/listParam")
+public ResponseEntity<?> getListParam(@RequestParam List<String> tag) {
+		tag.forEach(i -> System.out.println(i));
+		return null;
+}`;
+
+const request_param_map_params_multuple_keys = `@GetMapping(path = "/get/mapParam")
+public ResponseEntity<?> getMapParam(@RequestParam Map<Object, Object> params) {
+  	params.forEach((i,j) -> {			
+			System.out.println(i + " : " + j);
+		});
+	return null;
+}`;
