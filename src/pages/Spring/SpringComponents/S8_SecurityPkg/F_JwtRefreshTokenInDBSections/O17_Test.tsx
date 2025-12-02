ef4632beec6fc7655c13f27f1471f657cc986928
shabtay shalem <shabtay.shalem@gmail.com>
2025-12-02 17:57:20 +0200

    02-12-2025 17:57

--- src/pages/Spring/SpringComponents/S8_SecurityPkg/F_JwtRefreshTokenInDBSections/O17_Test.tsx
+++ src/pages/Spring/SpringComponents/S8_SecurityPkg/F_JwtRefreshTokenInDBSections/O17_Test.tsx
@@ -2,12 +2,11 @@
 
 
 */
-import { Anchor, IMG, MainChildArea } from "../../../../../components";
+import { Anchor, IMG, MainChildArea, ULDecimal } from "../../../../../components";
 import ULdisc from "../../../../../components/ui/ULdisc";
 import Li from "../../../../../components/ui/Li";
-import { SpanRed } from "../../../../../components/Highlight";
 import SpanGrey from "../../../../../components/Highlight/SpanGrey";
-import security_11 from "../../../../../assets/security_11.jpg";
+import security_refresh_test_1 from "../../../../../assets/security_refresh_test_1.jpg";
 import security_12 from "../../../../../assets/security_12.jpg";
 
 const O17_Test = ({ anchor }: { anchor: string }) => {
@@ -17,11 +16,7 @@ const O17_Test = ({ anchor }: { anchor: string }) => {
         <p className="my-4 text-lg font-semibold">🔑 GitHub link</p>
         <ULdisc>
           <Li>
-            🔑 GitHub project link ⇨{" "}
-            <Anchor
-              description="Spring boot Version v2.6.11 - Test_App"
-              href="https://github.com/sshalem/Spring-Boot/tree/main/08-Spring-Security/03_JWT/O2-jwt-authorities-v2-6-11#2_13_Test_App"
-            ></Anchor>{" "}
+            🔑 GitHub project link ⇨ <Anchor description="O4-jwt-refresh-token-DB - Test_App" href=""></Anchor>{" "}
           </Li>
         </ULdisc>
       </section>
@@ -32,20 +27,22 @@ const O17_Test = ({ anchor }: { anchor: string }) => {
         <article>
           <ULdisc>
             <Li>
-              Run project of
+              Run project of &nbsp;
               <Anchor
-                description="Spring boot Version v2.6.11 - O2-jwt-authorities-v2-6-11"
-                href="https://github.com/sshalem/Spring-Boot/tree/main/08-Spring-Security/03_JWT/O2-jwt-authorities-v2-6-11"
+                description="O4-jwt-refresh-token-DB"
+                href="https://github.com/sshalem/Spring-Boot/tree/main/08-Spring-Security/03_JWT/O4-jwt-refresh-token-DB"
               ></Anchor>{" "}
             </Li>
             <Li>
-              <SpanRed>Note</SpanRed> : I have <SpanGrey>data-H2.sql</SpanGrey> , <SpanGrey>data-MySql.sql</SpanGrey> ,{" "}
-              <SpanGrey>data-PostGreSql.sql</SpanGrey> to initialize the DB with some data . (depends on which DB I'm connected to)
+              App already with data initialized (See <SpanGrey>data-H2.sql , data-MySql.sql , data-PostGreSql.sql</SpanGrey> ){" "}
             </Li>
+
             <Li>
-              Send Login (Post request)
-              <IMG img_name={security_11}></IMG>
+              Set the <SpanGrey>JWT_EXPIRATION_TIME_ms = 5_000</SpanGrey> (in <SpanGrey>JwtTokenUtil</SpanGrey> ) to 5 secs. &nbsp; This ensures{" "}
+              <SpanGrey>accessToken</SpanGrey>
+              will expire after 5 secs.
             </Li>
+
             <Li>
               Notice to the expiration of the token , we can modify it in the code <SpanGrey>JwtTokenUtil</SpanGrey> class.
             </Li>
@@ -70,11 +67,47 @@ const O17_Test = ({ anchor }: { anchor: string }) => {
                   ) , to verify the signature
                 </Li>
                 <IMG img_name={security_12}></IMG>
+                <ULDecimal>
+                  <Li>
+                    Send Login (Post request)
+                    <IMG img_name={security_refresh_test_1}></IMG>
+                  </Li>
+                  <Li>
+                    send <SpanGrey>getUserByName</SpanGrey> <strong>few times</strong> till we get Expiration Error
+                    {/* <IMG img_name={security_refresh_stateless_3}></IMG> */}
+                  </Li>
+                  <Li>
+                    send <SpanGrey>refreshToken</SpanGrey> request, see that new <SpanGrey>accessToken</SpanGrey> generated, but{" "}
+                    <SpanGrey>refreshToken</SpanGrey> remain the same.
+                    {/* <IMG img_name={security_refresh_stateless_4}></IMG> */}
+                  </Li>
+                  <Li>
+                    send <SpanGrey>getUserByName</SpanGrey> <strong>right away</strong> (because we have a 5 sec gap before expiring) , to see if new
+                    token is valid.
+                  </Li>
+                  <Li>
+                    If valid , send till I get again <SpanGrey>JWT expired </SpanGrey> , because I want to see what App do if{" "}
+                    <SpanGrey>refreshToken</SpanGrey> is expired
+                  </Li>
+                  <Li>
+                    Send again <SpanGrey>refreshToken</SpanGrey>, we got refreshToken expired. the Logic is working great
+                    {/* <IMG img_name={security_refresh_stateless_5}></IMG> */}
+                  </Li>
+                </ULDecimal>
               </ULdisc>
             </Li>
           </ULdisc>
         </article>
       </section>
+
+      {/*  */}
+      {/*  */}
+      <ULdisc>
+        <Li>
+          With <SpanGrey>Postman</SpanGrey> I will sent this request in certain order as below
+          {/* <IMG img_name={security_refresh_stateless_1}></IMG> */}
+        </Li>
+      </ULdisc>
     </MainChildArea>
   );
 };
