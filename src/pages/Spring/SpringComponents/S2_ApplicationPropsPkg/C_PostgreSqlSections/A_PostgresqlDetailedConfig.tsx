8fb6fed24121914045bcab473c96faac75a73791
shabtay shalem <shabtay.shalem@gmail.com>
2025-12-04 11:10:54 +0200

    04-12-2025 11:10

--- src/pages/Spring/SpringComponents/S2_ApplicationPropsPkg/C_PostgreSqlSections/A_PostgresqlDetailedConfig.tsx
+++ src/pages/Spring/SpringComponents/S2_ApplicationPropsPkg/C_PostgreSqlSections/A_PostgresqlDetailedConfig.tsx
@@ -36,7 +36,6 @@ spring.datasource.password=admin
 # Spring will create a schema
 spring.jpa.hibernate.ddl-auto=create
 spring.jpa.generate-ddl=true
-spring.jpa.show-sql=true
 spring.jpa.properties.hibernate.generate_statistics=true
 
 # console color enabled
@@ -53,22 +52,10 @@ spring.jpa.properties.hibernate.format_sql=true
 logging.level.org.hibernate.SQL=DEBUG
 logging.level.org.hibernate.type.descriptor.sql.BasicBinder=TRACE
 
-# if this is not set to false i get 
-# java.lang.reflect.InvocationTargetException: null - when I play the project
-spring.jpa.properties.hibernate.temp.use_jdbc_metadata_defaults=false
-spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.PostgreSQL95Dialect
+# Allows Hibernate to generate SQL optimized for a particular DBMS
+spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.PostgreSQLDialect
 
 # ===========================================
 # 	dev-tools (Need to add dependency)
 # ===========================================
-spring.devtools.restart.enabled=true
-
-# ==========================================
-# format the returned error format when exception is thrown back to client
-# ==========================================
-server.error.path=true
-server.error.include-binding-errors=always
-server.error.include-exception=true
-server.error.include-message=always
-server.error.include-stacktrace=never
-server.error.whitelabel.enabled=true`;
+spring.devtools.restart.enabled=true`;
