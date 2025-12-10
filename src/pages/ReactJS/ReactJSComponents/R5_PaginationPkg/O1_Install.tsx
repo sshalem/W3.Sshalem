02e4658f05c893270c6a06b5b5b035305f72e40e
shabtay shalem <shabtay.shalem@gmail.com>
2025-12-10 21:13:43 +0200

    10-12-2025 21:13

--- src/pages/ReactJS/ReactJSComponents/R5_PaginationPkg/O1_Install.tsx
+++ src/pages/ReactJS/ReactJSComponents/R5_PaginationPkg/O1_Install.tsx
@@ -2,48 +2,65 @@
 
 
 */
-import { Anchor, GitHub, GitHubLiAnchor, MainChildArea } from "../../../../components";
-import { DivDoubleBorder, SpanBlue, SpanGreen, SpanRed } from "../../../../components/Highlight";
+import { Anchor, GitHub, GitHubLiAnchor, Li, MainChildArea, ULDecimal, ULdisc } from "../../../../components";
+import { DivDoubleBorder, JsxHighlight, SpanGreen, SpanGrey, SpanRed } from "../../../../components/Highlight";
 
 const O1_Install = ({ anchor }: { anchor: string }) => {
   return (
     <MainChildArea anchor={anchor}>
       <section>
-        I implemented my own <SpanBlue>Pagaination</SpanBlue> + <SpanBlue>Search</SpanBlue> functoinality. <br />
-        <ul className="my-4 ml-8 list-disc">
-          <li className="my-1">
-            Must install <SpanRed>Tailwindcss</SpanRed> and <SpanRed>react-icons</SpanRed> , because I used them in my implementation.
-          </li>
-          <li className="my-1">
-            Code is written in a way , that data will be itterated in the main Compnent where I config it (<strong>See next examples</strong> ), that
-            I can style the data as I want
-          </li>
-          <li className="my-1">
-            If I want to re-style the way that Pagination looks , Go inside <SpanGreen>Pagination.tsx</SpanGreen> code re-style it
-          </li>
-          <li className="my-1">
-            <SpanBlue>Pagaination</SpanBlue> works only when I get <SpanGreen>Array of objects</SpanGreen> from Backend, CSV, file , etc...
-          </li>
-          <li className="my-1">It can handle Array with a Single Object, because it's still an array</li>
-          <li className="my-1">
-            It will <SpanRed>NOT work on a single object</SpanRed>, it must be wrapped inside in <SpanBlue>Array</SpanBlue>
-          </li>
-        </ul>
+        In order to prevent using all kind of libraries , that do pagination , sort, search, <br />
+        I decided to implemented my own functionallity of: <br />
+        <ULDecimal>
+          <Li>Pagaination</Li>
+          <Li>Sort</Li>
+          <Li>Search</Li>
+        </ULDecimal>
+        <hr />
+        <article className="my-8 text-2xl font-semibold">🔑 Install steps notes</article>
+        <ULDecimal>
+          <Li>
+            Must install <SpanGrey>Tailwindcss</SpanGrey> , because I use it in my implementation.
+          </Li>
+          <Li>
+            Must install <SpanGrey>react-icons</SpanGrey> , because I use it in my implementation.
+          </Li>
+          <Li>
+            <SpanGrey>Pagaination</SpanGrey> works <SpanGrey>only</SpanGrey> when I get <SpanGrey>Array of objects</SpanGrey> from Backend, CSV, file
+            , etc...
+          </Li>
+          <Li>
+            Even If I have only 1 Object, I must handle it, as Array with a Single Object, because it's still an array (Code won't work with objects ,
+            only with arrays)
+          </Li>
+          <Li>
+            It will <SpanRed>NOT work on a single object</SpanRed>, it must be wrapped inside in <SpanGrey>Array</SpanGrey>
+          </Li>
+          <Li>
+            data is itterated inside the <SpanGrey>Pagination</SpanGrey> component
+          </Li>
+          <Li>Below, is where data is itterated in is the simplest way I style the data</Li>
+          <Li>
+            To re-style the way data looks, Go inside <SpanGrey>Pagination.tsx</SpanGrey> ,in last <strong>div</strong> tag , where{" "}
+            <strong>dataChuck</strong> is itterated
+          </Li>
+          <JsxHighlight jsxCode={data_re_style}></JsxHighlight>
+        </ULDecimal>
       </section>
 
       <DivDoubleBorder>Install</DivDoubleBorder>
-      <ul className="my-4 ml-8 list-disc">
-        <li className="my-1">
+      <ULdisc>
+        <Li>
           Create <SpanGreen>Pagaination.tsx</SpanGreen> component, in <SpanGreen>components</SpanGreen> folder .
-        </li>
-        <li className="my-1">
+        </Li>
+        <Li>
           Copy/Paste the code from{" "}
           <Anchor
             description="Pagination.tsx (from Project 20)"
             href="https://github.com/sshalem/ReactJS/blob/main/08-pagination-sort-items/src/20-Pagination-prod/Pagination.tsx"
           ></Anchor>
-        </li>
-      </ul>
+        </Li>
+      </ULdisc>
       <GitHub>
         <GitHubLiAnchor
           description="main - pagaination git"
@@ -63,3 +80,14 @@ const O1_Install = ({ anchor }: { anchor: string }) => {
 };
 
 export default O1_Install;
+
+const data_re_style = `      <div className="mx-auto grid w-7/12 grid-cols-2 py-2">
+        {dataChunk.map((itemProp: any, index: number) => {
+          const { id, first_name, last_name, email } = itemProp;
+          return (
+            <div key={index}>
+              {id} , {first_name} , {last_name} , {email}
+            </div>
+          );
+        })}
+      </div>`;
