c13491fe882cf69822f10321262ea1fd990a1c37
shabtay shalem <shabtay.shalem@gmail.com>
2026-01-04 15:55:33 +0200

    04-01-2026 15:55

--- src/pages/Python/PythonComponents/P1_PythonGeneralPkg/B_PythonGit/OX_CloneGitPythonProject.tsx
+++ src/pages/Python/PythonComponents/P1_PythonGeneralPkg/B_PythonGit/OX_CloneGitPythonProject.tsx
@@ -3,27 +3,44 @@
 
 */
 
-import { Li, MainChildArea, ULDecimal, ULdisc } from "../../../../../components";
-import { ApplicationPropertiesHighlight } from "../../../../../components/Highlight";
+import { IMG, Li, MainChildArea, ULdisc } from "../../../../../components";
+import { ApplicationPropertiesHighlight, SpanGrey } from "../../../../../components/Highlight";
+import python_7 from "../../../../../assets/python_7.jpg";
 
 const OX_CloneGitPythonProject = ({ anchor }: { anchor: string }) => {
   return (
     <MainChildArea anchor={anchor}>
       <section className="my-8">
         <article className="my-4 text-lg font-semibold">✅ Correct Workflow in PyCharm (BEST PRACTICE)</article>
-        <ULDecimal>
-          <Li>Open CMD and Clone the Project</Li>
+        <ULdisc>
+          <Li>1️⃣ Clone the repo</Li>
+          <Li>2️⃣ Open the project in PyCharm , (PyCharm detects no interpreter)</Li>
           <Li>
-            Configure the Virtual Environment in PyCharm (Key Step).
+            3️⃣ Create <SpanGrey>(.venv)</SpanGrey> folder , Open PyCharm Terminal , type command below (This creates a <SpanGrey>(.venv)</SpanGrey>
+            folder inside your project.)
+            <ApplicationPropertiesHighlight propertiesCode={_1_} />
+          </Li>
+
+          <Li>
+            4️⃣ Assign the <SpanGrey>(.venv)</SpanGrey> to PyCharm as the project interpreter
             <ULdisc>
-              Set it manually (recommended) In PyCharm :
-              <ULdisc>
-                <Li>File → Settings → Python → Interpreter</Li>
-                <Li>Click Add Interpreter</Li>
-                <Li>Choose Virtualenv Environment</Li>
-                <Li>Select: Existing environment</Li>
-              </ULdisc>
+              <Li>Go to :File → Settings → Python → Interpreter</Li>
+              <Li>Click Add Interpreter → Add Local Interpreter</Li>
+              <Li>
+                <strong>Environment</strong> : Existing Environment
+              </Li>
+              <Li>
+                <strong>Type</strong> : Virtualenv
+              </Li>
+              <Li>
+                <strong>Base Python</strong> : Path where Python.exe located (<SpanGrey>C:\Program Files\Python\Python312\python.exe</SpanGrey> )
+              </Li>
+              <IMG img_name={python_7}></IMG>
             </ULdisc>
+          </Li>
+          {/* <Li>
+            4️⃣ <SpanGrey>sanity check</SpanGrey> : Open PyCharm Terminal , type command below , → should point to <SpanGrey>(.venv)</SpanGrey>
+            <ApplicationPropertiesHighlight propertiesCode={_3_} />
           </Li>
           <Li>
             Install Dependencies (Two Correct Options), Open PyCharm Terminal, run command below:
@@ -32,8 +49,8 @@ const OX_CloneGitPythonProject = ({ anchor }: { anchor: string }) => {
           <Li>
             🧠 How to Verify It’s Working , In PyCharm Terminal, run:
             <ApplicationPropertiesHighlight propertiesCode={_2_} />
-          </Li>
-        </ULDecimal>
+          </Li> */}
+        </ULdisc>
       </section>
     </MainChildArea>
   );
@@ -41,6 +58,13 @@ const OX_CloneGitPythonProject = ({ anchor }: { anchor: string }) => {
 
 export default OX_CloneGitPythonProject;
 
-const _1_ = `pip install -r requirements.txt`;
+const _1_ = `python -m venv .venv`;
 
-const _2_ = `where python`;
+const _4_ = `pip install -r requirements.txt`;
+
+const _5_ = `where python`;
+
+const _6_ = `python -c "import sys; print(sys.executable)
+
+# correct output : .../MyApp/.venv/Scripts/python.exe
+# wrong output   : C:/\Program Files/\Python312/\python.exe`;
