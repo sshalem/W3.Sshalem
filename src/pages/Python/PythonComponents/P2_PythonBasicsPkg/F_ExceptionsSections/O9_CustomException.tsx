c82a59d2f8df0d3783017d2d6997b6fe16d88a59
shabtay shalem <shabtay.shalem@gmail.com>
2026-01-29 17:28:20 +0200

    29-01-2026 17:28

--- src/pages/Python/PythonComponents/P2_PythonBasicsPkg/F_ExceptionsSections/O9_CustomException.tsx
+++ src/pages/Python/PythonComponents/P2_PythonBasicsPkg/F_ExceptionsSections/O9_CustomException.tsx
@@ -10,14 +10,51 @@ const O9_CustomException = ({ anchor }: { anchor: string }) => {
     <MainChildArea anchor={anchor}>
       <section className="my-8">
         To Create our own custom Exception we will need to use a class to do that.
+        <br />
+        There are 2 ways to create a custom Exception
         <ULdisc>
-          <Li>create a class</Li>
           <Li>
-            extend from <SpanGrey>Exception class</SpanGrey>
+            <strong>
+              <em>
+                <mark>Option 1 (Best practice)</mark>
+              </em>
+            </strong>
+            <ULdisc>
+              <Li>
+                extend from <SpanGrey>Exception class</SpanGrey>
+                <PythonHighlight pythonCode={_1_} />
+              </Li>
+              <Li>
+                The message I pass , Python’s base <SpanGrey>Exception</SpanGrey> is stored internally and returned
+              </Li>
+              <Li>✔ Clean </Li>
+              <Li>✔ Matches Python + FastAPI best practices</Li>
+
+              <Li>
+                then raise the exception
+                <PythonHighlight pythonCode={_3_} />
+              </Li>
+            </ULdisc>
           </Li>
-          <Li>create a default constructor with attribute of msg</Li>
+
+          <Li>
+            <strong>
+              <em>
+                <mark>Option 2</mark>
+              </em>
+            </strong>
+            <ULdisc>
+              <Li>
+                extend from <SpanGrey>Exception class</SpanGrey>
+              </Li>
+              <Li>
+                <Li>create a default constructor with attribute of msg</Li>
+              </Li>
+              Here is how to use it
+              <PythonHighlight pythonCode={_2_} />
+            </ULdisc>
+          </Li>
         </ULdisc>
-        <PythonHighlight pythonCode={_1_} />
       </section>
     </MainChildArea>
   );
@@ -25,11 +62,16 @@ const O9_CustomException = ({ anchor }: { anchor: string }) => {
 export default O9_CustomException;
 
 const _1_ = `class CustomAmountException(Exception):
-    def __init__(self, msg):
-        self.msg = msg
+    pass
+`;
 
+const _2_ = `class CustomAmountException(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(message)
+`;
 
-def check(amount):
+const _3_ = `def check(amount):
     if amount > 20:
         raise CustomAmountException("the amount is wrong")
 
