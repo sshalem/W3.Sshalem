8f1908ecfb369058471db985473bcd4e151884a1
Shabtay Shalem <shabtay.shalem@asml.com>
2026-02-11 07:29:36 +0200

    02-11-2026  7:29

--- src/pages/FastAPI/FastAPIComponents/F1_FastAPIConfigurationProfilesEnvPkg/A_SqliteSections/O3_Database_Py.tsx
+++ src/pages/FastAPI/FastAPIComponents/F1_FastAPIConfigurationProfilesEnvPkg/A_SqliteSections/O3_Database_Py.tsx
@@ -4,41 +4,49 @@
 */
 
 import { Li, MainChildArea, ULdisc } from "../../../../../components";
-import { PythonHighlight, SpanYellow } from "../../../../../components/Highlight";
+import { JavaHighlight, PythonHighlight, SpanYellow } from "../../../../../components/Highlight";
 
 const O3_Database_Py = ({ anchor }: { anchor: string }) => {
   return (
     <MainChildArea anchor={anchor}>
       <section className="my-8">
         <article className="text-lg font-semibold"></article>
+        In <SpanYellow>database.py</SpanYellow> I define how to connect to DB, the Session, DecalrativeBase
+        <article className="my-8 text-lg font-semibold">
+          <span className="rounded-md border-2 border-gray-400 p-1">⭐ 1. Database engine</span>
+        </article>
         <ULdisc>
           <Li>
-            create <SpanYellow>database.py</SpanYellow> file
+            Database engine (same as Spring boot <SpanYellow> DataSource + Dialect + Connection Pool + Execution Layer</SpanYellow> )
           </Li>
+          <Li>Execution (in Python you execute via Engine/Connection; in Java you’d use JdbcTemplate or Hibernate)</Li>
+        </ULdisc>
+        <PythonHighlight pythonCode={_1_} />
+        <article className="my-8 text-lg font-semibold">
+          <span className="rounded-md border-2 border-gray-400 p-1">⭐ 2. Session</span>
+        </article>
+        <ULdisc>
           <Li>
-            this <SpanYellow>config.py</SpanYellow> will have
-            <ULdisc>
-              <Li>
-                Database engine (same as Spring boot <SpanYellow> DataSource + Dialect + Connection Pool + Execution Layer</SpanYellow> )
-                <ULdisc>
-                  <Li>Execution (in Python you execute via Engine/Connection; in Java you’d use JdbcTemplate or Hibernate)</Li>
-                </ULdisc>
-              </Li>
-              <Li>
-                Session ORM unit of work (same as Spring boot <SpanYellow>EntityManager (EntityManagerFactory)</SpanYellow>)
-                <ULdisc>
-                  <Li>Session (SQLAlchemy ORM) ≈ EntityManager</Li>
-                </ULdisc>
-              </Li>
-              <Li>
-                Base (DeclarativeBase) A shared ORM base class and mapper registry + metadata
-                <ULdisc>
-                  <Li>In Spring boot , JPA entity system managed by the EntityManagerFactory; entities are POJOs annotated with @Entity.</Li>
-                </ULdisc>
-              </Li>
-            </ULdisc>
+            Session ORM unit of work (same as Spring boot <SpanYellow>EntityManager (EntityManagerFactory)</SpanYellow>)
           </Li>
-          {/* <PythonHighlight pythonCode={_1_} /> */}
+          <Li>Session (SQLAlchemy ORM) ≈ EntityManager</Li>
+        </ULdisc>
+        <JavaHighlight javaCode={_2_} />
+        <article className="my-8 text-lg font-semibold">
+          <span className="rounded-md border-2 border-gray-400 p-1">⭐ 3. Base (DeclarativeBase)</span>
+        </article>
+        <ULdisc>
+          <Li>Base (DeclarativeBase) A shared ORM base class and mapper registry + metadata</Li>
+          <Li>In Spring boot , JPA entity system managed by the EntityManagerFactory; entities are POJOs annotated with @Entity.</Li>
+        </ULdisc>
+        <JavaHighlight javaCode={_3_} />
+        <article className="my-8 text-lg font-semibold">
+          <span className="rounded-md border-2 border-gray-400 p-1">⭐ 4. Dependency for DB session</span>
+        </article>
+        <ULdisc>
+          <Li>Dependency for DB session</Li>
+          <Li>FastAPI uses dependency injection, not magic annotations.</Li>
+          <PythonHighlight pythonCode={_4_} />
         </ULdisc>
       </section>
     </MainChildArea>
@@ -47,10 +55,23 @@ const O3_Database_Py = ({ anchor }: { anchor: string }) => {
 
 export default O3_Database_Py;
 
-const _1_ = `app/
-├── main.py                    # like SpringBootApplication
-│
-├── core
-│   ├── __init__.py
-│   ├── config.py              # application.properties / application.yml
-│   └── database.py            # DataSource + EntityManagerFactory`;
+const _1_ = `engine = create_engine(
+    settings.DATABASE_URL,
+    echo=settings.ECHO_SHOW_SQL,  # → print every SQL statement to the console
+    connect_args={"check_same_thread": False}  # → SQLite feature only
+)`;
+
+const _2_ = `SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)`;
+
+const _3_ = `class Base(DeclarativeBase):
+    pass`;
+
+const _4_ = `# same as 
+# @PersistenceContext
+# EntityManager em
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()`;
