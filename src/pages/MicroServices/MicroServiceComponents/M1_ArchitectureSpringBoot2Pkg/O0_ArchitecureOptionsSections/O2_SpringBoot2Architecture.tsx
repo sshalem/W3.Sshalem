3046913fbb1754b249b655d508bc4c07ddc1f9fb
shabtay shalem <shabtay.shalem@gmail.com>
2026-02-26 12:50:18 +0200

    26-02-2026 12:50

--- src/pages/MicroServices/MicroServiceComponents/M1_ArchitectureSpringBoot2Pkg/O0_ArchitecureOptionsSections/O2_SpringBoot2Architecture.tsx
+++ src/pages/MicroServices/MicroServiceComponents/M1_ArchitectureSpringBoot2Pkg/O0_ArchitecureOptionsSections/O2_SpringBoot2Architecture.tsx
@@ -4,6 +4,7 @@
 */
 import { IMG, Li, MainChildArea, ULDecimal, ULdisc } from "../../../../../components";
 import { SpanYellow } from "../../../../../components/Highlight";
+import microservice_1 from "../../../../../assets/microservice_1.jpg";
 
 const O2_SpringBoot2Architecture = ({ anchor }: { anchor: string }) => {
   return (
@@ -17,22 +18,51 @@ const O2_SpringBoot2Architecture = ({ anchor }: { anchor: string }) => {
             <Li>✅ Eureka</Li>
             <Li>✅ API Gateway</Li>
             <Li>✅ Config Server</Li>
-            <Li>✅ Load-balanced instances</Li>
+            <Li>
+              ✅ Load-balanced
+              <ULdisc>
+                <Li>Ribbon was used (now dead)</Li>
+                <Li>Use Spring Cloud LoadBalancer</Li>
+              </ULdisc>
+            </Li>
             <Li>✅ Separate DB per service</Li>
             <Li>✅ Resilience4j</Li>
-            <Li>✅ Sleuth + Zipkin (distributed tracing stack) standard tracing solution for Spring Boot 2 microservices</Li>
+            <Li>
+              ✅ Observability Sleuth + Zipkin (distributed tracing stack) standard tracing solution for Spring Boot 2 microservices
+              <ULdisc>
+                <Li>
+                  More adavanced <strong>is 3 pillars of observability</strong>
+                </Li>
+                <Li>
+                  🔹 Metrics stack(to monitor JVM memory, CPU , Request latency , Error rates ,Circuit breaker states), add:
+                  <ULdisc>
+                    <Li>Micrometer</Li>
+                    <Li>Prometheus</Li>
+                    <Li>Grafana</Li>
+                  </ULdisc>
+                </Li>
+              </ULdisc>
+            </Li>
             <Li>✅ Communication - OpenFeign , Kafka (Event driven), RabbitMq , RestTemplate (Old)</Li>
+            <Li>✅ ELK logging (Elasticsearch + Logstash + Kibana)</Li>
+            <Li>✅ Security</Li>
           </ULDecimal>
-          Why? This architecture is excellent to understand:
-          <ULdisc>
-            <Li>It teaches you the foundations.</Li>
-            <Li>Distributed systems</Li>
-            <Li>Observability</Li>
-            <Li>Resilience patterns</Li>
-            <Li>Service discovery</Li>
-            <Li>Cloud config management</Li>
-          </ULdisc>
-          <IMG img_name=""></IMG>
+          For <strong>Spring Boot 2</strong> microservices (2020–2023 era), This is a very correct and{" "}
+          <SpanYellow>production-grade architecture</SpanYellow>.
+          <article className="my-8">
+            🧠 Important Insight :This stack is not about Spring Boot version anymore.
+            <br />
+            It’s about:
+            <ULdisc>
+              <Li>Distributed systems principles</Li>
+              <Li>Observability</Li>
+              <Li>Resilience</Li>
+              <Li>Decoupling</Li>
+              <Li>Security</Li>
+            </ULdisc>
+            <SpanYellow>That knowledge transfers 100% to Spring Boot 3</SpanYellow>.
+          </article>
+          <IMG img_name={microservice_1}></IMG>
         </article>
       </section>
     </MainChildArea>
