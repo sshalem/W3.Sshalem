ba93eb980373058257926b947be99f068d13f158
shabtay shalem <shabtay.shalem@gmail.com>
2026-09-16 14:42:53 +0300

    02-9/- 14:42

--- Git_all.bat
+++ Git_all.bat
@@ -21,6 +21,11 @@ if %ComputerName%==LAPTOP-AAAA (
     echo.   [36mcomputer name is = %ComputerName% [0m
     echo.   [36m%DATE% , %TIME%	 [0m
     git commit -m "%DATE:~7,2%-%DATE:~4,2%-%DATE:~10,4% %TIME:~0,2%:%TIME:~3,2%"   
+) else if %ComputerName%==LAPTOP-LENOVO-S (
+    echo.[33mgit commit -m "%DATE:~7,2%-%DATE:~4,2%-%DATE:~10,4% %TIME:~0,2%:%TIME:~3,2%" [0m
+    echo.   [36mcomputer name is = %ComputerName% [0m
+    echo.   [36m%DATE% , %TIME%	 [0m
+    git commit -m "%DATE:~7,2%-%DATE:~4,2%-%DATE:~10,4% %TIME:~0,2%:%TIME:~3,2%"   
 ) else if %ComputerName%==LAPTOP-ASUS-SHA (
     echo.[33mgit commit -m "%DATE:~0,2%-%DATE:~3,2%-%DATE:~6,4% %TIME:~0,2%:%TIME:~3,2%"  [0m
     @REM echo.[33mgit commit -m "%DATE:~7,2%-%DATE:~4,2%-%DATE:~10,4% %TIME:~0,2%:%TIME:~3,2%" [0m
